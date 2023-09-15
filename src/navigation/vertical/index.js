da91eb9676f0825d52cfd94ad3a15c0acf1ab258
Naseer <naseer@trellisys.net>
2023-09-15 13:45:32 +0530

    Changes related to files and folder structure and navigation.

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -14,7 +14,7 @@ const navigation = () => {
       icon: 'mdi:home-outline',
       children: [
         {
-          title: 'Supplier',
+          title: 'Suppliers',
           icon: 'mdi:email-outline',
           path: '/supplier'
         },
