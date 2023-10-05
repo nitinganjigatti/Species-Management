cc64e2438007c59771816953d245cd0e956c3641
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2023-10-05 19:04:12 +0530

    added state add and update module

--- src/lib/api/getStates.js
+++ src/lib/api/getStates.js
@@ -34,3 +34,110 @@ export async function getStates() {
       // throw new Error(error);
     })
 }
+
+export async function addState(payload) {
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${STATES}`
+  console.log('url', url)
+  debugger
+  console.log('params: ', payload)
+
+  return axios({
+    method: 'post',
+    url: url,
+    data: payload,
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+    .then(response => {
+      console.log('State POST DATA', response)
+
+      return response?.data
+    })
+    .catch(error => {
+      console.error(url)
+      if (error.response) {
+        console.info('Request made and server responded')
+        console.error(error.response.data)
+        console.error(error.response.status)
+        console.error(error.response.headers)
+      } else if (error.request) {
+        console.info('The request was made but no response was received')
+        console.error(error.request)
+      } else {
+        console.info('Something happened in setting up the request that triggered an Error')
+        console.error('Error', error.message)
+      }
+
+      return error
+    })
+}
+
+// /state/1/show
+export async function getStateById(id) {
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${STATES}/${id}/show`
+
+  return axios
+    .get(url)
+    .then(response => {
+      console.log('response data', response)
+
+      return response
+    })
+    .then(response => {
+      console.log('State POST DATA', response)
+
+      return response?.data
+    })
+    .catch(error => {
+      console.error(url)
+      if (error.response) {
+        console.info('Request made and server responded')
+        console.error(error.response.data)
+        console.error(error.response.status)
+        console.error(error.response.headers)
+      } else if (error.request) {
+        console.info('The request was made but no response was received')
+        console.error(error.request)
+      } else {
+        console.info('Something happened in setting up the request that triggered an Error')
+        console.error('Error', error.message)
+      }
+
+      return error
+    })
+}
+
+export async function updateStates(id, payload) {
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${STATES}/${id}/update`
+  console.log('url', url)
+  debugger
+  console.log('params: ', payload)
+
+  return axios({
+    method: 'post',
+    url: url,
+    data: payload,
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+    .then(response => {
+      console.log('Generic Update DATA', response)
+
+      return response?.data
+    })
+    .catch(error => {
+      console.error(url)
+      if (error.response) {
+        console.info('Request made and server responded')
+        console.error(error.response.data)
+        console.error(error.response.status)
+        console.error(error.response.headers)
+      } else if (error.request) {
+        console.info('The request was made but no response was received')
+        console.error(error.request)
+      } else {
+        console.info('Something happened in setting up the request that triggered an Error')
+        console.error('Error', error.message)
+      }
+
+      return error
+    })
+}
--- src/pages/pharmacy/medicine/state/index.js
+++ src/pages/pharmacy/medicine/state/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { getStates } from 'src/lib/api/getStates'
+import { getStates, addState, updateStates } from 'src/lib/api/getStates'
 import TableWithFilter from 'src/components/TableWithFilter'
 import Button from '@mui/material/Button'
 import FallbackSpinner from 'src/@core/components/spinner/index'
@@ -15,10 +15,76 @@ import { Box, Drawer } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 
 import Router from 'next/router'
+import AddStates from 'src/views/pages/pharmacy/medicine/state/addState'
+import UserSnackbar from 'src/components/utility/snackbar'
 
 const ListOfStates = () => {
   const [stateList, setStateList] = useState([])
   const [loader, setLoader] = useState(false)
+
+  /*** Drawer ****/
+  const editParamsInitialState = { id: null, name: null, status: null, code: null, short_code: null }
+  const [openDrawer, setOpenDrawer] = useState(false)
+  const [resetForm, setResetForm] = useState(false)
+  const [submitLoader, setSubmitLoader] = useState(false)
+  const [editParams, setEditParams] = useState(editParamsInitialState)
+
+  const [openSnackbar, setOpenSnackbar] = useState({
+    open: false,
+    severity: '',
+    message: ''
+  })
+
+  const addEventSidebarOpen = () => {
+    console.log('event clicked')
+    setEditParams({ id: null, name: null, status: null, code: null, short_code: null })
+    setResetForm(true)
+    console.log('edit', editParams)
+    setOpenDrawer(true)
+  }
+
+  const handleSidebarClose = () => {
+    console.log('close event clicked')
+    setOpenDrawer(false)
+  }
+
+  const handleSubmitData = async payload => {
+    console.log('payload', payload)
+    try {
+      setSubmitLoader(true)
+      var response
+      if (editParams?.id !== null) {
+        response = await updateStates(editParams?.id, payload)
+      } else {
+        response = await addState(payload)
+      }
+
+      if (response?.success) {
+        setOpenSnackbar({ ...openSnackbar, open: true, message: response?.message, severity: 'success' })
+        setSubmitLoader(false)
+        setResetForm(true)
+        setOpenDrawer(false)
+
+        await getStatesLists()
+      } else {
+        setSubmitLoader(false)
+        console.log('test')
+        setOpenSnackbar({ ...openSnackbar, open: true, message: response?.message?.name, severity: 'error' })
+      }
+    } catch (e) {
+      console.log(e)
+      setSubmitLoader(false)
+      setOpenSnackbar({ ...openSnackbar, open: true, message: 'Error', severity: 'error' })
+    }
+  }
+
+  const handleEdit = async (id, name, code, short_code, status) => {
+    console.log('in state file', id, name, code, short_code, status)
+    setEditParams({ id: id, name: name, code: code, short_code: short_code, status: status })
+    setOpenDrawer(true)
+  }
+
+  /***** Drawer  */
 
   const getStatesLists = async () => {
     setLoader(true)
@@ -38,8 +104,6 @@ const ListOfStates = () => {
       setLoader(false)
     }
   }
-
-  const handleEdit = async (id, name, status) => {}
 
   useEffect(() => {
     getStatesLists()
@@ -109,7 +173,14 @@ const ListOfStates = () => {
       headerName: 'Action',
       renderCell: params => (
         <Box sx={{ display: 'flex', alignItems: 'right', textAlign: 'right' }}>
-          <IconButton size='small' sx={{ mr: 0.5 }} onClick={() => handleEdit(params.row.id)} aria-label='Edit'>
+          <IconButton
+            size='small'
+            sx={{ mr: 0.5 }}
+            onClick={() =>
+              handleEdit(params.row.id, params.row.name, params.row.code, params.row.short_code, params.row.status)
+            }
+            aria-label='Edit'
+          >
             <Icon icon='mdi:pencil-outline' />
           </IconButton>
         </Box>
@@ -127,7 +198,7 @@ const ListOfStates = () => {
             TableTitle={stateList.length > 0 ? 'State List' : 'State list is empty add State'}
             headerActions={
               <div>
-                <Button size='big' variant='contained'>
+                <Button size='big' variant='contained' onClick={() => addEventSidebarOpen()}>
                   Add State
                 </Button>
               </div>
@@ -135,6 +206,18 @@ const ListOfStates = () => {
             columns={columns}
             rows={stateList}
           />
+          <AddStates
+            drawerWidth={400}
+            addEventSidebarOpen={openDrawer}
+            handleSidebarClose={handleSidebarClose}
+            handleSubmitData={handleSubmitData}
+            resetForm={resetForm}
+            submitLoader={submitLoader}
+            editParams={editParams}
+          />
+          {openSnackbar.open ? (
+            <UserSnackbar severity={openSnackbar?.severity} status={true} message={openSnackbar?.message} />
+          ) : null}
         </>
       )}
     </>
