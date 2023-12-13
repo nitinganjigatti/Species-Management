8bd21943066e753cebc9caced80d73ca2da7649d
Naseer <naseer@trellisys.net>
2023-12-13 11:02:42 +0530

    Changes related to Request.

--- src/lib/api/getRequestItemsList.js
+++ src/lib/api/getRequestItemsList.js
@@ -46,31 +46,6 @@ export async function getDispatchItemsByBatchId(id) {
   return response.data
 }
 
-export async function shipRequestedItems(payload) {
-  try {
-    const url = `${SHIPMENT}`
-    var data = payload
-    const response = await axiosPost({ url, body: data })
-
-    return response?.data
-  } catch (error) {
-    if (error.response) {
-      console.info('Request made and server responded')
-      console.error(error.response.data)
-      console.error(error.response.status)
-      console.error(error.response.headers)
-    }
-
-    return error
-  }
-}
-
-export async function getShippedItemsByRequestId(id) {
-  const response = await axiosGet({ url: `${SHIPMENT}/${id}/show` })
-
-  return response.data
-}
-
 // export async function getRequestItemsListById(id) {
 //   const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${REQUEST_ITEMS}/${id}/edit`
 //   console.log('url', url)
@@ -206,3 +181,51 @@ export async function updateRequestItems(id, payload) {
 //       // throw new Error(error);
 //     })
 // }
+
+// Shipment
+
+export async function shipRequestedItems(payload) {
+  try {
+    const url = `${SHIPMENT}`
+    var data = payload
+    const response = await axiosPost({ url, body: data })
+
+    return response?.data
+  } catch (error) {
+    if (error.response) {
+      console.info('Request made and server responded')
+      console.error(error.response.data)
+      console.error(error.response.status)
+      console.error(error.response.headers)
+    }
+
+    return error
+  }
+}
+
+export async function getShippedItemsByRequestId(id) {
+  const response = await axiosGet({ url: `${SHIPMENT}/${id}/show` })
+
+  return response.data
+}
+
+export async function updateShipmentRequest(id, payload) {
+  try {
+    const url = `${SHIPMENT}/${id}/update`
+    var data = payload
+    data.id = id
+    const response = await axiosPost({ url, body: data })
+
+    return response?.data
+  } catch (error) {
+    console.error(url)
+    if (error.response) {
+      console.info('Request made and server responded')
+      console.error(error.response.data)
+      console.error(error.response.status)
+      console.error(error.response.headers)
+    }
+
+    return error
+  }
+}
