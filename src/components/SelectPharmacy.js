df93c2595a238f62f2ef2e3611366ef1600e3113
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2023-12-21 16:32:36 +0530

    removed unwanted commented lines

--- src/components/SelectPharmacy.js
+++ src/components/SelectPharmacy.js
@@ -36,7 +36,6 @@ function SelectPharmacy() {
     const storedPharmacy = await readAsync('selectedStore')
 
     // console.log('storedPharmacy', storedPharmacy)
-    // console.log('options', options)
 
     const foundStored = () => {
       return options.some(item => item.id === storedPharmacy?.id)
