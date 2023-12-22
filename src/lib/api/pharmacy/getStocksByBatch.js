58d2c00b21f491c64ac739ffba458e5a8270b402
Naseer <naseer@trellisys.net>
2023-12-22 15:09:54 +0530

    Changes related to Pharmacy.

--- src/lib/api/pharmacy/getStocksByBatch.js
+++ src/lib/api/pharmacy/getStocksByBatch.js
@@ -2,7 +2,7 @@ import { STOCK_BY_BATCH } from '../../../constants/ApiConstant'
 import { axiosGet, axiosPost } from '../utility'
 
 export async function getStocksByBatch(id) {
-  const response = await axiosGet({ url: `${STOCK_BY_BATCH}/${id}?page=1&limit=25&search=&` })
+  const response = await axiosGet({ url: `${STOCK_BY_BATCH}/${id}?page=1&limit=25&search=&`, pharmacy: true })
 
   return response.data
 }
