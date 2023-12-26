04883c9f7c4505cab0c16a40556b7f2e6ee510c3
Naseer <naseer@trellisys.net>
2023-12-26 17:16:43 +0530

    Changes router names.

--- src/pages/pharmacy/purchase/purchase-list/index.js
+++ src/pages/pharmacy/purchase/purchase-list/index.js
@@ -195,7 +195,7 @@ const ListOfPurchase = () => {
                         size='big'
                         variant='contained'
                         onClick={() => {
-                          Router.push('/pharmacy/purchase/addPurchase/')
+                          Router.push('/pharmacy/purchase/add-purchase/')
                         }}
                       >
                         Add Purchase
