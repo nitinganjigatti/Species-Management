baf3fa6a3499ec4d1fb4b246184b843f45f7c8fe
Naseer <naseer@trellisys.net>
2023-12-26 12:55:26 +0530

    Changes related to purchase.

--- src/pages/pharmacy/purchase/add-purchase/index.js
+++ src/pages/pharmacy/purchase/add-purchase/index.js
@@ -1,23 +1,7 @@
 import AddPurchaseForm from 'src/components/pharmacy/purchase/AddPurchaseForm'
-import Error404 from 'src/pages/404'
-
-import { usePharmacyContext } from 'src/context/PharmacyContext'
 
 const AddPurchase = () => {
-  const { selectedPharmacy } = usePharmacyContext()
-
-  return (
-    <>
-      {selectedPharmacy.type === 'central' &&
-      (selectedPharmacy.permission.key === 'allow_full_access' || selectedPharmacy.permission.key === 'ADD') ? (
-        <AddPurchaseForm />
-      ) : (
-        <>
-          <Error404></Error404>
-        </>
-      )}
-    </>
-  )
+  return <AddPurchaseForm />
 }
 
 export default AddPurchase
