f73b6b84d76940d0b9ca1cdbb02a9a92a48b4480
Naseer <naseer@trellisys.net>
2023-12-26 15:50:29 +0530

    Added permissions to manufacturer master.

--- src/pages/pharmacy/settings/manufacturer/index.js
+++ src/pages/pharmacy/settings/manufacturer/index.js
@@ -23,6 +23,9 @@ import AddManufacturer from 'src/views/pages/pharmacy/medicine/manufacturers/add
 import UserSnackbar from 'src/components/utility/snackbar'
 import ServerSideToolbar from 'src/views/table/data-grid/ServerSideToolbar'
 
+import { usePharmacyContext } from 'src/context/PharmacyContext'
+import Error404 from 'src/pages/404'
+
 const ManufacturerList = () => {
   const [manufacturers, setManufacturers] = useState({})
   const [loader, setLoader] = useState(false)
@@ -37,6 +40,8 @@ const ManufacturerList = () => {
   const [openSnackbar, setOpenSnackbar] = useState(false)
   const [snackbarMessage, setSnackbarMessage] = useState('')
   const [severity, setSeverity] = useState('success')
+
+  const { selectedPharmacy } = usePharmacyContext()
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
@@ -108,17 +113,22 @@ const ManufacturerList = () => {
       field: 'Action',
       headerName: 'Action',
       renderCell: params => (
-        <Box sx={{ display: 'flex', alignItems: 'right', textAlign: 'right' }}>
-          {parseInt(params.row.zoo_id) === 0 ? null : (
-            <IconButton
-              size='small'
-              sx={{ mr: 0.5 }}
-              onClick={() => handleEdit(params.row.id, params.row.label, params.row.active)}
-            >
-              <Icon icon='mdi:pencil-outline' />
-            </IconButton>
-          )}
-        </Box>
+        <>
+          {selectedPharmacy.type === 'central' &&
+            (selectedPharmacy.permission.key === 'allow_full_access' || selectedPharmacy.permission.key === 'ADD') && (
+              <Box sx={{ display: 'flex', alignItems: 'right', textAlign: 'right' }}>
+                {parseInt(params.row.zoo_id) === 0 ? null : (
+                  <IconButton
+                    size='small'
+                    sx={{ mr: 0.5 }}
+                    onClick={() => handleEdit(params.row.id, params.row.label, params.row.active)}
+                  >
+                    <Icon icon='mdi:pencil-outline' />
+                  </IconButton>
+                )}
+              </Box>
+            )}
+        </>
       )
     }
   ]
@@ -194,9 +204,12 @@ const ManufacturerList = () => {
 
   const headerAction = (
     <div>
-      <Button size='big' variant='contained' onClick={() => addEventSidebarOpen()}>
-        Add Manufacturer
-      </Button>
+      {selectedPharmacy.type === 'central' &&
+        (selectedPharmacy.permission.key === 'allow_full_access' || selectedPharmacy.permission.key === 'ADD') && (
+          <Button size='big' variant='contained' onClick={() => addEventSidebarOpen()}>
+            Add Manufacturer
+          </Button>
+        )}
     </div>
   )
 
@@ -237,48 +250,61 @@ const ManufacturerList = () => {
 
   return (
     <>
-      {loader ? (
-        <FallbackSpinner />
+      {selectedPharmacy.type === 'central' ? (
+        <>
+          {loader ? (
+            <FallbackSpinner />
+          ) : (
+            <>
+              <Card>
+                <CardHeader title='Manufacturers' action={headerAction} />
+                <DataGrid
+                  autoHeight
+                  pagination
+                  rows={indexedRows === undefined ? [] : indexedRows}
+                  rowCount={total}
+                  columns={columns}
+                  sortingMode='server'
+                  paginationMode='server'
+                  pageSizeOptions={[7, 10, 25, 50]}
+                  paginationModel={paginationModel}
+                  onSortModelChange={handleSortModel}
+                  slots={{ toolbar: ServerSideToolbar }}
+                  onPaginationModelChange={setPaginationModel}
+                  loading={loading}
+                  slotProps={{
+                    baseButton: {
+                      variant: 'outlined'
+                    },
+                    toolbar: {
+                      value: searchValue,
+                      clearSearch: () => handleSearch(''),
+                      onChange: event => handleSearch(event.target.value)
+                    }
+                  }}
+                />
+              </Card>
+              <AddManufacturer
+                drawerWidth={400}
+                addEventSidebarOpen={openDrawer}
+                handleSidebarClose={handleSidebarClose}
+                handleSubmitData={handleSubmitData}
+                resetForm={resetForm}
+                submitLoader={submitLoader}
+                editParams={editParams}
+              />
+              <UserSnackbar
+                status={openSnackbar}
+                message={snackbarMessage}
+                severity={severity}
+                handleClose={handleClose}
+              />
+            </>
+          )}
+        </>
       ) : (
         <>
-          <Card>
-            <CardHeader title='Manufacturers' action={headerAction} />
-            <DataGrid
-              autoHeight
-              pagination
-              rows={indexedRows === undefined ? [] : indexedRows}
-              rowCount={total}
-              columns={columns}
-              sortingMode='server'
-              paginationMode='server'
-              pageSizeOptions={[7, 10, 25, 50]}
-              paginationModel={paginationModel}
-              onSortModelChange={handleSortModel}
-              slots={{ toolbar: ServerSideToolbar }}
-              onPaginationModelChange={setPaginationModel}
-              loading={loading}
-              slotProps={{
-                baseButton: {
-                  variant: 'outlined'
-                },
-                toolbar: {
-                  value: searchValue,
-                  clearSearch: () => handleSearch(''),
-                  onChange: event => handleSearch(event.target.value)
-                }
-              }}
-            />
-          </Card>
-          <AddManufacturer
-            drawerWidth={400}
-            addEventSidebarOpen={openDrawer}
-            handleSidebarClose={handleSidebarClose}
-            handleSubmitData={handleSubmitData}
-            resetForm={resetForm}
-            submitLoader={submitLoader}
-            editParams={editParams}
-          />
-          <UserSnackbar status={openSnackbar} message={snackbarMessage} severity={severity} handleClose={handleClose} />
+          <Error404></Error404>
         </>
       )}
     </>
