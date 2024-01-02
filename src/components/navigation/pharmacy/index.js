78f7bacb685c47ee0bc7fbd6b31452e083563008
Naseer <naseer@trellisys.net>
2024-01-02 20:15:55 +0530

    Changes related to navigation and rack

--- src/components/navigation/pharmacy/index.js
+++ src/components/navigation/pharmacy/index.js
@@ -209,10 +209,10 @@ const composePharmacyNavigation = ({ pharmacyList, pharmacyRole, selectedPharmac
   if (selectedPharmacy?.type === 'local') {
     requestParent.children.push(requestListing)
     returnParent.children.push(returnListing, addReturnRequest)
-    stockParent.children.push(stockReport, stockReportByBatch, stockOut, expiredMedicine)
+    stockParent.children.push(stockReport, stockOut, expiredMedicine)
+    directDispatchParent.children.push(directDispatchList)
     settingsParent.children.push(rackList)
-
-    pharmacyNavigationArray.push(requestParent, returnParent, stockParent, settingsParent)
+    pharmacyNavigationArray.push(requestParent, returnParent, directDispatchParent, stockParent, settingsParent)
   }
 
   console.log(pharmacyNavigationArray)
