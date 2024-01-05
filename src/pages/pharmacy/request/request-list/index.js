4d129a0756a19d838a00059eb6e1aa29607f81d6
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2024-01-05 16:32:21 +0530

    data updating issue fixed

--- src/pages/pharmacy/request/request-list/index.js
+++ src/pages/pharmacy/request/request-list/index.js
@@ -115,6 +115,11 @@ const RequestList = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchTableData, status, selectedPharmacy])
 
+  useEffect(() => {
+    fetchTableData(sort, searchValue, sortColumn, status)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedPharmacy.id])
+
   const getSlNo = index => (paginationModel.page + 1 - 1) * paginationModel.pageSize + index + 1
 
   const indexedRows = rows?.map((row, index) => ({
