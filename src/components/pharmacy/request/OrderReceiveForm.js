3fb3f8b287ee6a3ea7fac318052b2e8a8a56e9f2
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2024-01-17 18:26:56 +0530

    changes related to order receive form

--- src/components/pharmacy/request/OrderReceiveForm.js
+++ src/components/pharmacy/request/OrderReceiveForm.js
@@ -15,9 +15,6 @@ import toast from 'react-hot-toast'
 
 import {
   getShipmentOrderDetails,
-  addDisputeItems,
-  addDispenseItems,
-  getDisputeItemById,
   getShipmentStatusList,
   resolveDisputeItems
 } from 'src/lib/api/pharmacy/getShipmentList'
@@ -425,9 +422,11 @@ function OrderReceiveForm({ orderId, requestId, closeOrderFormDialog }) {
                 </Grid>
               ) : (
                 <Grid container>
-                  {((params.row.status === 'Missing' || params.row.status === 'Wrong Count') &&
+                  {((params.row.status === 'Missing' ||
+                    params.row.status === 'Wrong Count' ||
+                    params.row.status === '') &&
                     params?.row?.dispute_status === 'Not Resolved') ||
-                  (params.row.status === '' && params.row.id !== '') ? (
+                  !params?.row?.dispute_status ? (
                     <Grid xs={12} sm={12}>
                       <FormControl fullWidth size='small'>
                         <Select
