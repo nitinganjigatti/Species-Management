e9625c8bc492c9938765ffb196e9113510bb52a2
Naseer <naseer@trellisys.net>
2024-03-11 18:23:11 +0530

    Fixed issue AF-1802

--- src/components/pharmacy/purchase/AddPurchaseForm.js
+++ src/components/pharmacy/purchase/AddPurchaseForm.js
@@ -943,7 +943,7 @@ const AddPurchaseForm = () => {
                     >
                       {suppliers?.map(item => (
                         <MenuItem key={item.id} disabled={item.status === 'inactive'} value={item.id}>
-                          {item.name}
+                          {item.company_name}
                         </MenuItem>
                       ))}
                     </Select>
