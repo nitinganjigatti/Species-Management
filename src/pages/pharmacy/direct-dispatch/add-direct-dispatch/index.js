7daf2443084efb7261f22389e5f59e48d7e230b7
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2024-03-18 15:05:53 +0530

    changes related to direct dispatch request

--- src/pages/pharmacy/direct-dispatch/add-direct-dispatch/index.js
+++ src/pages/pharmacy/direct-dispatch/add-direct-dispatch/index.js
@@ -98,7 +98,9 @@ const initialNestedRowMedicine = {
   priority_item: 'Normal',
   control_substance: false,
   control_substance_file: '',
-  uuid: ''
+  uuid: '',
+  stock_type: ''
+
   // to_store_id: '14'
 }
 
@@ -369,7 +371,8 @@ const AddReturnRequest = () => {
           searchResults?.data?.list_items?.map(item => ({
             value: item.id,
             label: item.name,
-            control_substance: item.controlled_substance === '1' ? true : false
+            control_substance: item.controlled_substance === '1' ? true : false,
+            stock_type: item.stock_type
           }))
         )
       }
@@ -380,12 +383,12 @@ const AddReturnRequest = () => {
     }
   }
 
-  const fetchBatchData = async id => {
+  const fetchBatchData = async (id, productType) => {
     if (id !== '') {
       try {
         setBatchLoading(true)
         const data = { stock_item_id: id }
-        const searchResults = await getAvailableMedicineByMedicineId(id, data, 'central')
+        const searchResults = await getAvailableMedicineByMedicineId(id, data, 'central', productType)
 
         if (searchResults?.success) {
           if (searchResults?.data?.items.length > 0) {
@@ -427,9 +430,9 @@ const AddReturnRequest = () => {
   }, [])
 
   const searchBatchData = useCallback(
-    debounce(async id => {
+    debounce(async (id, productType) => {
       try {
-        await fetchBatchData(id)
+        await fetchBatchData(id, productType)
       } catch (error) {
         console.error(error)
       }
@@ -471,7 +474,8 @@ const AddReturnRequest = () => {
             expiry_date: el.dispatch_expiry_date,
             uuid: uuidv4(),
             available_item_qty: el?.batch_available_qty,
-            dispatch_item_id: el.dispatch_item_id
+            dispatch_item_id: el.dispatch_item_id,
+            stock_type: el?.stock_type
           }
         })
 
@@ -534,7 +538,8 @@ const AddReturnRequest = () => {
       priority_item: getItems[0].priority_item,
       control_substance: getItems[0].control_substance,
       uuid: getItems[0].uuid,
-      available_item_qty: getItems[0]?.available_item_qty
+      available_item_qty: getItems[0]?.available_item_qty,
+      stock_type: getItems[0]?.stock_type
     })
     // }
   }
@@ -870,7 +875,7 @@ const AddReturnRequest = () => {
                           </TableCell>
                           <TableCell>
                             <Typography variant='body2' sx={{ color: 'text.primary' }}>
-                              {el.expiry_date}
+                              {Utility.formatDisplayDate(el.expiry_date) === 'Invalid date' ? 'NA' : el.expiry_date}
                             </Typography>
                           </TableCell>
                           <TableCell>{el.priority_item}</TableCell>
