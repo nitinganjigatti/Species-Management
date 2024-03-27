10a6d442b82fd752082d2439907005ef3e648881
Arushi Sharma <arushi@lifesciencetrust.com>
2024-03-27 18:02:10 +0530

    removed commented lines

--- src/utility/Confirm-dialog-box.js
+++ src/utility/Confirm-dialog-box.js
@@ -1,4 +1,3 @@
-import { LoadingButton } from '@mui/lab'
 import { Button, Grid, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 
@@ -10,32 +9,26 @@ export const ConfirmationBox = ({ setConfirmationBox }) => {
       <Grid container>
         <Grid item>
           <Typography variant='h6' sx={{ position: 'relative', left: '64px' }}>
-            {' '}
             Are you sure you want to Cancel ?
           </Typography>
         </Grid>
         <Grid xs={12}>
-          <Button sx={{ position: 'relative', left: '144px' }}>Yes</Button>
-          <Button sx={{ position: 'relative', left: '144px' }}>No</Button>
-        </Grid>
-      </Grid>
-      {/* <Grid container xs={12} sx={{ position: 'relative', bottom: '12px' }}>
-        <Typography variant='subtitle2' sx={{ margin: '4px', position: 'relative', left: '150px' }}>
-          Are you sure you want to Cancel ?
-        </Typography>
-        <Grid xs={12} sx={{ position: 'relative', left: '200px', top: '10px' }}>
-          <LoadingButton
-            sx={{ margin: '2px' }}
-            variant='contained'
+          <Button
+            variant='outlined'
+            sx={{ position: 'relative', left: '144px', m: '5px' }}
             onClick={() => router.push('/pharmacy/new-product-request/')}
           >
             Yes
-          </LoadingButton>
-          <LoadingButton variant='contained' sx={{ margin: '2px' }} onClick={() => setConfirmationBox(false)}>
+          </Button>
+          <Button
+            variant='outlined'
+            sx={{ position: 'relative', left: '144px' }}
+            onClick={() => setConfirmationBox(false)}
+          >
             No
-          </LoadingButton>
+          </Button>
         </Grid>
-      </Grid> */}
+      </Grid>
     </>
   )
 }
