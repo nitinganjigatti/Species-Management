abab553b0c293551283559ffbabc3153a340a1a8
Arushi Sharma <arushi@lifesciencetrust.com>
2024-04-02 10:47:41 +0530

    corrected the style of confirmation modal

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -1,7 +1,7 @@
 // ** React Imports
 import Dialog from '@mui/material/Dialog'
 import DialogContent from '@mui/material/DialogContent'
-import { Button, DialogActions, DialogContentText, DialogTitle } from '@mui/material'
+import { Button, DialogActions, DialogContentText, DialogTitle, Grid } from '@mui/material'
 import { Box } from '@mui/system'
 
 const ConfirmDialog = ({ title, closeDialog, open, action, content }) => {
@@ -19,17 +19,17 @@ const ConfirmDialog = ({ title, closeDialog, open, action, content }) => {
         <DialogTitle id='alert-dialog-title'>{title}</DialogTitle>
         {/* <DialogContentText sx={{ textAlign: 'center', pt: 3 }}>{title}</DialogContentText> */}
         <DialogContent>{content}</DialogContent>
-        <Box sx={{ display: 'flex', gap: 7 }}>
+        <Box sx={{ display: 'flex', gap: 7, justifyContent: 'flex-end' }}>
           {/* <DialogContentText sx={{ ml: 5 }}>Confirm to proceed</DialogContentText> */}
           <DialogActions className='dialog-actions-dense'>
-            <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+            <Grid>
               <Button sx={{ mr: 2 }} variant='contained' size='small' onClick={() => action()}>
                 Confirm
               </Button>
               <Button variant='outlined' size='small' onClick={() => closeDialog()}>
                 Close
               </Button>
-            </Box>
+            </Grid>
           </DialogActions>
         </Box>
       </Dialog>
