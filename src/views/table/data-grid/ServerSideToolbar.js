08f040892fe061a9c1fc8ba7024fcbb215cc7748
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2024-04-02 17:56:48 +0530

    issue fixed

--- src/views/table/data-grid/ServerSideToolbar.js
+++ src/views/table/data-grid/ServerSideToolbar.js
@@ -3,11 +3,18 @@ import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
 import { GridToolbarExport } from '@mui/x-data-grid'
+import { useRef } from 'react'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
 const ServerSideToolbar = props => {
+  const inputRef1 = useRef()
+
+  const handleFocus = () => {
+    inputRef1.current.focus()
+  }
+
   return (
     <Box
       sx={{
@@ -15,7 +22,7 @@ const ServerSideToolbar = props => {
         display: 'flex',
         flexWrap: 'wrap',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-end',
         p: theme => theme.spacing(2, 5, 4, 5)
       }}
     >
@@ -23,6 +30,8 @@ const ServerSideToolbar = props => {
       <TextField
         size='small'
         value={props.value}
+        inputRef={inputRef1}
+        onFocus={handleFocus}
         onChange={props.onChange}
         placeholder='Search…'
         InputProps={{
@@ -31,11 +40,21 @@ const ServerSideToolbar = props => {
               <Icon icon='mdi:magnify' fontSize={20} />
             </Box>
           ),
-          endAdornment: (
-            <IconButton size='small' title='Clear' aria-label='Clear' onClick={props.clearSearch}>
-              <Icon icon='mdi:close' fontSize={20} />
-            </IconButton>
-          )
+
+          endAdornment:
+            props.value === '' ? (
+              <IconButton
+                size='small'
+                sx={{ color: 'transparent', opacity: '-1', cursor: 'text' }}
+                onClick={handleFocus}
+              >
+                <Icon icon='mdi:close' fontSize={20} sx={{ color: 'transparent' }} />
+              </IconButton>
+            ) : (
+              <IconButton size='small' title='Clear' aria-label='Clear' onClick={props.clearSearch}>
+                <Icon icon='mdi:close' fontSize={20} />
+              </IconButton>
+            )
         }}
         sx={{
           width: {
