33017b5b23013b3595a194aa6f396714a6225af0
Naseer <naseer@trellisys.net>
2024-04-23 18:12:14 +0530

    Removed lint while doing a build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
@@ -8,6 +9,11 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
