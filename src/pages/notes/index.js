8405af4a0e656a970d394c0b0cae272c8a3a186e
Naseer <naseer@trellisys.net>
2024-05-02 11:35:48 +0530

    Started notes module.

---
+++ src/pages/notes/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function NotesDashboard(props) {
+  return <div>Notes Dashboard</div>
+}
+
+export default NotesDashboard
---
+++ src/pages/notes/masters/notes-listing/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NotesListing(props) {
+  return <div>Notes Listing</div>
+}
+
+export default NotesListing
