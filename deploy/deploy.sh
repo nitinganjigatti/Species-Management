e60ae04a30ea07beda98da66c3b9b2ae3d78fc87
Nidhin Pratap <nidhin@trellisys.net>
2024-05-21 18:58:51 +0530

    deployment fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -31,7 +31,6 @@ elif [ $GITHUB_REF_NAME == 'prod-release' ]
 then
   DEPLOY_SERVER=$DEPLOY_SERVER_PROD
   ENV_TO_LOAD='production'
-fi
 elif [ $GITHUB_REF_NAME == 'release/farm' ]
 then
   DEPLOY_SERVER=$DEPLOY_SERVER_PROD
