292c3f4579ebdc1aed7a56fde01e7dfb357419e4
Nihal kishore <nihal.desuntechnology@gmail.com>
2024-05-31 12:58:38 +0530

    Room Ui

--- src/pages/egg/incubator-rooms/index.js
+++ src/pages/egg/incubator-rooms/index.js
@@ -1,7 +1,336 @@
-import React from 'react'
+import {
+  Avatar,
+  Box,
+  Breadcrumbs,
+  Button,
+  Card,
+  CardHeader,
+  Stack,
+  Tooltip,
+  Typography,
+  Link,
+  Grid
+} from '@mui/material'
+import React, { useCallback, useState } from 'react'
+import Icon from 'src/@core/components/icon'
+import { DataGrid } from '@mui/x-data-grid'
+import { useTheme } from '@mui/material/styles'
+import ServerSideToolbarWithFilter from 'src/views/table/data-grid/ServerSideToolbarWithFilter'
+import CustomChip from 'src/@core/components/mui/chip'
+import { debounce } from 'lodash'
+import Router from 'next/router'
+import FallbackSpinner from 'src/@core/components/spinner/index'
 
 const RoomsList = () => {
-  return <div>Rooms List</div>
+  const theme = useTheme()
+  const [loader, setLoader] = useState(false)
+  const [total, setTotal] = useState(0)
+  const [sort, setSort] = useState('desc')
+  const [rows, setRows] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [sortColumning, setsortColumning] = useState('ingredient_name')
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+  const [loading, setLoading] = useState(false)
+  const [status, setStatus] = useState('')
+
+  const headerAction = (
+    <>
+      <Box sx={{ display: 'flex', height: '32px', justifyContent: 'space-between' }}>
+        <Button
+          sx={{ px: 7, py: 5 }}
+          size='small'
+          variant='contained'
+
+          // onClick={() => Router.push('/egg/feed/')}
+        >
+          <Icon icon='mdi:add' fontSize={20} />
+          &nbsp; ADD NEW
+        </Button>
+      </Box>
+    </>
+  )
+
+  // const getSlNo = index => (paginationModel.page + 1 - 1) * paginationModel.pageSize + index + 1
+
+  const indexedRows = data?.map((row, index) => ({
+    ...row
+
+    // sl_no: getSlNo(index)
+  }))
+
+  const handleSortModel = newModel => {
+    // if (newModel.length) {
+    //   setSort(newModel[0].sort)
+    //   setsortColumning(newModel[0].field)
+    //   fetchTableData(newModel[0].sort, searchValue, newModel[0].field, status)
+    // } else {
+    // }
+  }
+
+  const searchTableData = useCallback(
+    debounce(async (sort, q, sortColumn, status) => {
+      setSearchValue(q)
+      try {
+        // await fetchTableData(sort, q, sortColumn, status)
+      } catch (error) {
+        console.error(error)
+      }
+    }, 1000),
+    []
+  )
+
+  const handleSearch = value => {
+    setSearchValue(value)
+    searchTableData(sort, value, sortColumning, status)
+  }
+
+  const columns = [
+    {
+      flex: 0.05,
+      Width: 40,
+      field: 'uid',
+      headerName: 'SL ',
+      renderCell: params => (
+        <Typography variant='body2' sx={{ color: 'text.primary' }}>
+          {params.row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.5,
+      minWidth: 30,
+      field: 'nursery_name',
+      headerName: 'nursery name',
+      renderCell: params => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography variant='body2' sx={{ color: 'text.primary' }}>
+            {params.row.nursery}
+          </Typography>
+        </Box>
+      )
+    },
+    {
+      flex: 0.3,
+      minWidth: 10,
+      field: 'site',
+      headerName: 'site',
+      renderCell: params => (
+        <Typography variant='body2' sx={{ color: 'text.primary' }}>
+          {params.row.site}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.3,
+      minWidth: 10,
+      field: 'room',
+      headerName: 'room',
+      renderCell: params => (
+        <Typography variant='body2' sx={{ color: 'text.primary' }}>
+          {params.row.room}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.4,
+      minWidth: 20,
+      field: 'Incubator',
+      headerName: 'Incubator',
+      renderCell: params => (
+        <Typography variant='body2' sx={{ color: 'text.primary' }}>
+          {params.row.Incubator}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.6,
+      minWidth: 60,
+      field: 'user_name',
+      headerName: 'CREATED BY',
+      renderCell: params => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {/* {renderClient(params)} */}
+          {/* <Avatar
+            variant='square'
+            alt='Medicine Image'
+            sx={{
+              width: 30,
+              height: 30,
+              mr: 4,
+              borderRadius: '50%',
+              background: '#E8F4F2',
+              overflow: 'hidden'
+            }}
+          >
+            {params.row.created_by_user?.profile_pic ? (
+              <img
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                src={params.row.created_by_user?.profile_pic}
+                alt='Profile'
+              />
+            ) : (
+              <Icon icon='mdi:user' />
+            )}
+          </Avatar>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography noWrap variant='body2' sx={{ color: 'text.primary', fontSize: 14 }}>
+              {params.row.created_by_user?.user_name ? params.row.created_by_user?.user_name : '-'}
+            </Typography>
+            <Typography noWrap variant='body2' sx={{ color: '#44544a9c', fontSize: 12 }}>
+              {params.row.created_at ? 'Created on' + ' ' + moment(params.row.created_at).format('DD/MM/YYYY') : '-'}
+            </Typography>
+          </Box> */}
+        </Box>
+      )
+    },
+    {
+      flex: 0.3,
+      minWidth: 10,
+      field: 'status',
+      headerName: 'STATUS',
+      renderCell: params => (
+        <Typography>Status</Typography>
+
+        // <CustomChip
+        //   skin='light'
+        //   size='small'
+        //   label={params.row?.active === '1' ? 'Active' : 'InActive'}
+        //   color={params.row?.active === '1' ? roleColors.active : roleColors.inactive}
+        //   sx={{
+        //     height: 20,
+        //     fontWeight: 600,
+        //     borderRadius: '5px',
+        //     fontSize: '0.875rem',
+        //     textTransform: 'capitalize',
+        //     '& .MuiChip-label': { mt: -0.25 }
+        //   }}
+        // />
+      )
+    }
+  ]
+
+  const onCellClick = params => {
+    console.log(params, 'params')
+    const clickedColumn = params.field !== 'switch'
+
+    if (clickedColumn) {
+      const data = params.row
+
+      Router.push({
+        pathname: `/egg/incubator-rooms/${data?.id}`
+      })
+    } else {
+      return
+    }
+  }
+
+  return (
+    <>
+      {loader ? (
+        <FallbackSpinner />
+      ) : (
+        <Box>
+          <Breadcrumbs aria-label='breadcrumb' sx={{ mb: 5 }}>
+            <Typography color='inherit'>Egg</Typography>
+            <Link underline='hover' color='text.primary' href='/egg/incubator-rooms/'>
+              Incubator Room
+            </Link>
+          </Breadcrumbs>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <Card sx={{ px: 5, py: 3 }}>
+                <CardHeader title='Incubator Rooms' action={headerAction} />
+
+                <Box sx={{ py: 4, px: 4 }}>
+                  <Stack direction='row' gap={3}>
+                    <Typography variant='h6'>Legends : </Typography>
+                    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
+                      <Box sx={{ px: 3.5, py: 3.5, bgcolor: '#F2F2F2', borderRadius: 1 }}></Box>
+                      <Typography variant='body1' sx={{ fontSize: '20px' }}>
+                        Disabled
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
+                      <Box sx={{ px: 3.5, py: 3.5, bgcolor: '#e1f9ed', borderRadius: 1 }}></Box>
+                      <Typography variant='body1' sx={{ fontSize: '20px' }}>
+                        Empty
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
+                      <Box sx={{ px: 3.5, py: 3.5, bgcolor: '#e1f9ed', borderRadius: 1 }}></Box>
+                      <Typography variant='body1' sx={{ fontSize: '20px' }}>
+                        Available
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
+                      <Box sx={{ px: 3.5, py: 3.5, bgcolor: '#afe5c3', borderRadius: 1 }}></Box>
+                      <Typography variant='body1' sx={{ fontSize: '20px' }}>
+                        No Slots Available
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
+                      <Box sx={{ px: 3.5, py: 3.5, bgcolor: '#ffe9e9', borderRadius: 1 }}></Box>
+                      <Typography variant='body1' sx={{ fontSize: '20px' }}>
+                        Alert
+                      </Typography>
+                    </Box>
+                  </Stack>
+                </Box>
+
+                <Box sx={{ px: 3 }}>
+                  <DataGrid
+                    sx={{
+                      '.MuiDataGrid-cell:focus': {
+                        outline: 'none'
+                      },
+
+                      '& .MuiDataGrid-row:hover': {
+                        cursor: 'pointer'
+                      }
+                    }}
+                    columnVisibilityModel={{
+                      sl_no: false
+                    }}
+                    hideFooterSelectedRowCount
+                    disableColumnSelector={true}
+                    autoHeight
+                    pagination
+                    rows={indexedRows === undefined ? [] : indexedRows}
+                    rowCount={total}
+                    columns={columns}
+                    sortingMode='server'
+                    paginationMode='server'
+                    pageSizeOptions={[7, 10, 25, 50]}
+                    paginationModel={paginationModel}
+                    onSortModelChange={handleSortModel}
+                    slots={{ toolbar: ServerSideToolbarWithFilter }}
+                    onPaginationModelChange={setPaginationModel}
+                    loading={loading}
+                    slotProps={{
+                      baseButton: {
+                        variant: 'outlined'
+                      },
+                      toolbar: {
+                        value: searchValue,
+                        clearSearch: () => handleSearch(''),
+                        onChange: event => handleSearch(event.target.value)
+                      }
+                    }}
+                    onCellClick={onCellClick}
+                  />
+                </Box>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      )}
+    </>
+  )
 }
 
 export default RoomsList
+
+const data = [
+  { id: '1', nursery: 'Nursery name', site: 'Site name', room: 'Room', Incubator: 'Incubator' },
+  { id: '2', nursery: 'Nursery name', site: 'Site name', room: 'Room', Incubator: 'Incubator' }
+]
