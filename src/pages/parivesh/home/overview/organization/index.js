d3f5852e830dc7fcec4d4632fd2a74b9e2952753
clear <anilrathod@lifesciencetrust.com>
2024-06-20 11:03:31 +0530

    overview batch list api integration

--- src/pages/parivesh/home/overview/organization/index.js
+++ src/pages/parivesh/home/overview/organization/index.js
@@ -1,141 +1,53 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react'
 
-import { getIngredientList } from 'src/lib/api/diet/getIngredients'
-
 import FallbackSpinner from 'src/@core/components/spinner/index'
 import CardHeader from '@mui/material/CardHeader'
 import { DataGrid } from '@mui/x-data-grid'
 import { debounce } from 'lodash'
-import Tab from '@mui/material/Tab'
-import TabPanel from '@mui/lab/TabPanel'
-import TabContext from '@mui/lab/TabContext'
-import { styled } from '@mui/material/styles'
-import MuiTabList from '@mui/lab/TabList'
-import TabList from '@mui/lab/TabList'
+
 import moment from 'moment'
 import { Avatar, Button, Tooltip, Box, Switch, Divider, CardContent } from '@mui/material'
-import CustomChip from 'src/@core/components/mui/chip'
 
 // ** MUI Imports
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
-import Chip from '@mui/material/Chip'
+
 import Grid from '@mui/material/Grid'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 import Router from 'next/router'
 import ServerSideToolbarWithFilter from 'src/views/table/data-grid/ServerSideToolbarWithFilter'
-import { updateIngredientStatus } from 'src/lib/api/diet/getIngredients'
+
 import ConfirmationDialog from 'src/components/confirmation-dialog'
 import ConfirmationCheckBox from 'src/views/forms/form-elements/confirmationCheckBox'
 import { useTheme } from '@mui/material/styles'
-import AddIngredients from 'src/components/diet/AddIngredients'
-import Error404 from 'src/pages/404'
 
 import { AuthContext } from 'src/context/AuthContext'
 import Toaster from 'src/components/Toaster'
-import CustomAccordion from 'src/components/parivesh/CustomAccordion'
+import { usePariveshContext } from 'src/context/PariveshContext'
+import { getBatchListSpecies } from 'src/lib/api/parivesh/batchListSpecies'
 
 const Organization = () => {
   const theme = useTheme()
   const [loader, setLoader] = useState(false)
   const [total, setTotal] = useState(0)
   const [sort, setSort] = useState('desc')
-  const [rows, setRows] = useState([
-    {
-      uid: '01',
-      id: '1',
-      registration_id: 'WL/GJ/132549',
-      of_species: '555',
-      of_animals: '2501',
-      created_at: '2024-06-03 16:07:17',
-      approved_date: '2024-06-06 16:07:17',
-      created_by_user: {
-        user_name: 'sr',
-        email: 'sr@mailinator.com',
-        profile_pic: 'https://api.dev.antzsystems.com/uploads/11/diet/ingredients/665d9cdd975011717411037.jpg'
-      }
-    }
-  ])
+  const [rows, setRows] = useState([])
+  const { selectedParivesh } = usePariveshContext()
   const [searchValue, setSearchValue] = useState('')
-  const [sortColumning, setsortColumning] = useState('registration_id')
+  const [sortColumn, setSortColumn] = useState('registration_id')
   const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
   const [loading, setLoading] = useState(false)
 
-  const [statusCheckval, setstatusCheckval] = useState(false)
   const [dialog, setDialog] = useState(false)
   const [check, setCheck] = useState(false)
-  const [selectedIngredient, setSelectedIngredient] = useState()
 
   const authData = useContext(AuthContext)
-  const dietModule = authData?.userData?.roles?.settings?.diet_module
-  const dietModuleAccess = authData?.userData?.roles?.settings?.diet_module_access
-  const [openIngredient, setOpenIngredient] = useState(false)
+
   function loadServerRows(currentPage, data) {
     return data
   }
-
-  const data = [
-    { value: 200, label: 'TOTAL ANIMALS', color: '#FFFFFF', borderColor: '#FFFFFF' },
-    { value: 103, label: 'MALE', color: '#00AFD6', borderColor: '#00AFD6' },
-    { value: 74, label: 'FEMALE', color: '#FFD3D3', borderColor: '#FFD3D3' },
-    { value: 23, label: 'OTHERS', color: '#FFFFFF', borderColor: '#FFFFFF' },
-    { value: 156, label: 'TOTAL SPECIES', color: '#E4B819', borderColor: '#E4B819' }
-  ]
-
-  const cards = [
-    {
-      value: 60,
-      content: 'Parent Stock',
-      bgColor: '#37BD69',
-      items: [
-        { value: 6, bgColor: '#00AFD6' },
-        { value: 5, bgColor: '#FFD3D3' },
-        { value: 10, bgColor: '#FFFFFF' }
-      ]
-    },
-    {
-      value: 25,
-      content: 'Acquisition',
-      bgColor: '#37BD69',
-      items: [
-        { value: 11, bgColor: '#00AFD6' },
-        { value: 7, bgColor: '#FFD3D3' },
-        { value: 6, bgColor: '#FFFFFF' }
-      ]
-    },
-    {
-      value: 5,
-      content: 'Births',
-      bgColor: '#37BD69',
-      items: [
-        { value: 21, bgColor: '#00AFD6' },
-        { value: 2, bgColor: '#FFD3D3' },
-        { value: 7, bgColor: '#FFFFFF' }
-      ]
-    },
-    {
-      value: 5,
-      content: 'Deaths',
-      bgColor: '#E93353',
-      items: [
-        { value: 2, bgColor: '#00AFD6' },
-        { value: 6, bgColor: '#FFD3D3' },
-        { value: 6, bgColor: '#FFFFFF' }
-      ]
-    },
-    {
-      value: 5,
-      content: 'Transfers',
-      bgColor: '#FA6140',
-      items: [
-        { value: 6, bgColor: '#00AFD6' },
-        { value: 11, bgColor: '#FFD3D3' },
-        { value: 3, bgColor: '#FFFFFF' }
-      ]
-    }
-  ]
 
   const handleChange = (event, newValue) => {
     setTotal(0)
@@ -146,44 +58,42 @@ const Organization = () => {
     setDialog(false)
   }
 
-  //   const fetchTableData = useCallback(
-  //     async (sort, q, sortColumn, status) => {
-  //       try {
-  //         setLoading(true)
+  const fetchTableData = useCallback(
+    async (sort, q, sortColumn) => {
+      try {
+        setLoading(true)
 
-  //         const params = {
-  //           sort,
-  //           q,
-  //           sortColumn,
-  //           page: paginationModel.page + 1,
-  //           limit: paginationModel.pageSize,
-  //           status
-  //         }
+        const params = {
+          q,
+          status: 'all',
+          page: paginationModel.page + 1,
+          sort,
+          sortColumn,
+          limit: paginationModel.pageSize,
+          org_id: selectedParivesh.id !== 'all' ? selectedParivesh.id : null
+        }
 
-  //         await getIngredientList({ params: params }).then(res => {
-  //           console.log('response', res)
+        await getBatchListSpecies({ params: params }).then(res => {
+          console.log('responseqq', res)
+          // Generate uid field based on the index
+          let listWithId = res.data.data.map((el, i) => {
+            return { ...el, id: i + 1 }
+          })
+          setTotal(parseInt(res?.data?.total_count))
+          setRows(loadServerRows(paginationModel.page, listWithId))
+        })
+        setLoading(false)
+      } catch (e) {
+        console.log(e)
+        setLoading(false)
+      }
+    },
+    [paginationModel, selectedParivesh]
+  )
 
-  //           // Generate uid field based on the index
-  //           let listWithId = res.data.result.map((el, i) => {
-  //             return { ...el, uid: i + 1 }
-  //           })
-  //           setTotal(parseInt(res?.data?.total_count))
-  //           setRows(loadServerRows(paginationModel.page, listWithId))
-  //         })
-  //         setLoading(false)
-  //       } catch (e) {
-  //         console.log(e)
-  //         setLoading(false)
-  //       }
-  //     },
-  //     [paginationModel]
-  //   )
-
-  //   useEffect(() => {
-  //     if (dietModule) {
-  //       fetchTableData(sort, searchValue, sortColumning, status)
-  //     }
-  //   }, [fetchTableData, status])
+  useEffect(() => {
+    fetchTableData(sort, searchValue, sortColumn)
+  }, [fetchTableData])
 
   const getSlNo = index => (paginationModel.page + 1 - 1) * paginationModel.pageSize + index + 1
 
@@ -192,41 +102,41 @@ const Organization = () => {
     sl_no: getSlNo(index)
   }))
 
-  //   const handleSortModel = newModel => {
-  //     if (newModel.length) {
-  //       setSort(newModel[0].sort)
-  //       setsortColumning(newModel[0].field)
-  //       fetchTableData(newModel[0].sort, searchValue, newModel[0].field, status)
-  //     } else {
-  //     }
-  //   }
+  const handleSortModel = newModel => {
+    if (newModel.length) {
+      setSort(newModel[0].sort)
+      setSortColumn(newModel[0].field)
+      fetchTableData(newModel[0].sort, searchValue, newModel[0].field)
+    } else {
+    }
+  }
 
-  //   const searchTableData = useCallback(
-  //     debounce(async (sort, q, sortColumn, status) => {
-  //       setSearchValue(q)
-  //       try {
-  //         await fetchTableData(sort, q, sortColumn, status)
-  //       } catch (error) {
-  //         console.error(error)
-  //       }
-  //     }, 1000),
-  //     []
-  //   )
+  const searchTableData = useCallback(
+    debounce(async (sort, q, sortColumn) => {
+      setSearchValue(q)
+      try {
+        await fetchTableData(sort, q, sortColumn)
+      } catch (error) {
+        console.error(error)
+      }
+    }, 1000),
+    []
+  )
 
-  //   const handleSearch = value => {
-  //     setSearchValue(value)
-  //     searchTableData(sort, value, sortColumning, status)
-  //   }
+  const handleSearch = value => {
+    setSearchValue(value)
+    searchTableData(sort, value, sortColumn)
+  }
 
   const columns = [
     {
       flex: 0.2,
       Width: 40,
-      field: 'uid',
+      field: 'id',
       headerName: 'S.NO',
       renderCell: params => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {params.row.uid}
+          {params.row.id}
         </Typography>
       )
     },
@@ -248,33 +158,33 @@ const Organization = () => {
     {
       flex: 0.3,
       minWidth: 10,
-      field: 'of_species',
+      field: 'species_count',
       headerName: '# OF SPECIES',
       renderCell: params => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {params.row.of_species ? params.row.of_species : '-'}
+          {params.row.species_count ? params.row.species_count : '-'}
         </Typography>
       )
     },
     {
       flex: 0.3,
       minWidth: 10,
-      field: 'of_animals',
+      field: 'no_of_animals',
       headerName: '# OF ANIMALS',
       renderCell: params => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {params.row.of_animals ? params.row.of_animals : '-'}
+          {params.row.no_of_animals ? params.row.no_of_animals : '-'}
         </Typography>
       )
     },
     {
       flex: 0.4,
       minWidth: 20,
-      field: 'approved_date',
+      field: 'accepted_on',
       headerName: 'Approved DATE',
       renderCell: params => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {params.row.approved_date ? moment(params.row.approved_date).format('DD/MM/YYYY') : '-'}
+          {params.row.accepted_on ? moment(params.row.accepted_on).format('DD/MM/YYYY') : '-'}
         </Typography>
       )
     },
@@ -389,7 +299,7 @@ const Organization = () => {
               paginationMode='server'
               pageSizeOptions={[7, 10, 25, 50]}
               paginationModel={paginationModel}
-              //   onSortModelChange={handleSortModel}
+              onSortModelChange={handleSortModel}
               slots={{ toolbar: ServerSideToolbarWithFilter }}
               onPaginationModelChange={setPaginationModel}
               loading={loading}
