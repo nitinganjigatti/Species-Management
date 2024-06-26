7bd08b48a0b30f4b6339f37568b9b60d33d94cee
Sandeep <sandeep.desuntech@gmail.com>
2024-06-26 18:21:33 +0530

    removed consoles

--- src/pages/egg/incubators/index.js
+++ src/pages/egg/incubators/index.js
@@ -90,9 +90,9 @@ const IncubatorsList = () => {
           // site_id: 14
           site_id: ''
         }
-        console.log('params', params)
+        // console.log('params', params)
         await getIncubatorList(params).then(res => {
-          console.log('response', res)
+          // console.log('response', res)
 
           // Generate uid field based on the index
           let listWithId = res?.data?.data?.result?.map((el, i) => {
