4b4ae432505e2af3cc4f9268421cd13cc6bcbb63
Nidhin Pratap <nidhin@trellisys.net>
2024-06-29 15:14:29 +0530

    debug the variables

--- deploy/fetchRepo.sh
+++ deploy/fetchRepo.sh
@@ -95,18 +95,21 @@ echo "Downloading artifact"
 ARTIFACT_NAME="nextjs-build-output"  # Replace with the name from your workflow
 GITHUB_RUN_ID="${GITHUB_RUN_ID}"  # Replace with an environment variable from your workflow
 
-# echo "${ANTZ_DEPLOYMENT_TOKEN}":x-oauth-basic https://artifacts.githubusercontent.com/v4/repos/${GITHUB_REPOSITORY}/workflows/${GITHUB_WORKFLOW}/runs/${GITHUB_RUN_ID}/artifacts/${ARTIFACT_NAME}
-# curl -L --user "${ANTZ_DEPLOYMENT_TOKEN}":x-oauth-basic https://artifacts.githubusercontent.com/v4/repos/${GITHUB_REPOSITORY}/workflows/${GITHUB_WORKFLOW}/runs/${GITHUB_RUN_ID}/artifacts/${ARTIFACT_NAME} > artifact.zip
+echo "${ANTZ_DEPLOYMENT_TOKEN}":x-oauth-basic https://artifacts.githubusercontent.com/v4/repos/${GITHUB_REPOSITORY}/workflows/${GITHUB_WORKFLOW}/runs/${GITHUB_RUN_ID}/artifacts/${ARTIFACT_NAME}
+curl -L --user "${ANTZ_DEPLOYMENT_TOKEN}":x-oauth-basic https://artifacts.githubusercontent.com/v4/repos/${GITHUB_REPOSITORY}/workflows/${GITHUB_WORKFLOW}/runs/${GITHUB_RUN_ID}/artifacts/${ARTIFACT_NAME} > artifact.zip
 
+# Get the artifact ID
+# ARTIFACT_ID=$(curl -s -H "Authorization: token $ANTZ_DEPLOYMENT_TOKEN" \
+#     "https://api.github.com/repos/$GITHUB_USERNAME/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/artifacts" \
+#     | jq -r ".artifacts[] | select(.name == \"$ARTIFACT_NAME\") | .id")
 
-# # Replace with actual values
-ARTIFACT_URL="https://artifacts.githubusercontent.com/v4/repos/${GITHUB_REPOSITORY}/workflows/${GITHUB_WORKFLOW}/runs/${GITHUB_RUN_ID}/artifacts/${ARTIFACT_NAME}"
-echo $ARTIFACT_URL;
-# Download the artifact with authentication
-echo "curl -v -L --user '${ANTZ_DEPLOYMENT_TOKEN}':x-oauth-basic -o artifact.zip '$ARTIFACT_URL'"
+# # Download the artifact
+# curl -L -H "Authorization: token $ANTZ_DEPLOYMENT_TOKEN" \
+#     -o $ARTIFACT_NAME.zip \
+#     "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/actions/artifacts/$ARTIFACT_ID/zip"
 
-curl -v -L --user "${ANTZ_DEPLOYMENT_TOKEN}":x-oauth-basic -o artifact.zip "$ARTIFACT_URL"
-
+# Unzip the artifact
+unzip $ARTIFACT_NAME.zip -d $ARTIFACT_NAME
 
 ls -lt
 # Optionally verify the download
