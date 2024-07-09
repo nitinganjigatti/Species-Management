75f102d70e11fc73a1d9f8d77b1b708a49ac31e3
Arushi Sharma <arushi@lifesciencetrust.com>
2024-07-09 16:56:52 +0530

    set the response message in the allocation slider

--- src/views/pages/egg/eggs/allocationSlider.js
+++ src/views/pages/egg/eggs/allocationSlider.js
@@ -232,7 +232,7 @@ const AllocationSlider = ({ setOpenDrawer, allocateEggId, callApi, allocationVal
       }
       const response = await AddAllocation(params)
       if (response.success) {
-        Toaster({ type: 'success', message: 'Egg Allocated Successfully' })
+        Toaster({ type: 'success', message: response.message })
 
         if (callApi) {
           callApi('')
