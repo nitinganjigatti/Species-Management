b9f6bfd97695afb1d013cbad261b20e583d09870
clear <anilrathod@lifesciencetrust.com>
2024-07-11 11:12:23 +0530

    media ui changes

--- src/pages/media/index.js
+++ src/pages/media/index.js
@@ -21,16 +21,10 @@ import {
   Divider,
   Menu
 } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete'
 import { LoadingButton } from '@mui/lab'
 import { useTheme } from '@mui/material/styles'
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'
-import DescriptionIcon from '@mui/icons-material/Description'
-import ImageIcon from '@mui/icons-material/Image'
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
 import SearchIcon from '@mui/icons-material/Search'
 import EventIcon from '@mui/icons-material/Event'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 import { useDropzone } from 'react-dropzone'
 import { useAuth } from 'src/hooks/useAuth'
@@ -38,12 +32,7 @@ import Icon from 'src/@core/components/icon'
 import Toaster from 'src/components/Toaster'
 import { deleteMediaFile, getMediaListById, uploadMediaFile } from 'src/lib/api/media'
 import moment from 'moment'
-
-import pdfIcon from 'public/icons/pdf_icon.svg'
-import xlsIcon from 'public/icons/xls_icon.svg'
-import docIcon from 'public/icons/doc_icon.svg'
 import Image from 'next/image'
-import { isSameDay } from 'date-fns'
 
 const Media = () => {
   const auth = useAuth()
@@ -56,8 +45,11 @@ const Media = () => {
   const [selectedDateFilter, setSelectedDateFilter] = useState('all') // Initialize date filter state
   const [selectedFileTypeFilter, setSelectedFileTypeFilter] = useState('all') // Initialize file type filter state
   const [searchQuery, setSearchQuery] = useState('')
+  const [anchorEl, setAnchorEl] = useState(null)
 
   const userId = auth?.userData?.user?.user_id
+
+  const imgPath = auth?.userData?.settings?.DEFAULT_IMAGE_MASTER
 
   const getMediaListUserId = useCallback(
     async userId => {
@@ -119,14 +111,14 @@ const Media = () => {
   })
 
   const handleDelete = async id => {
-    console.log(selectedId, 'delete')
-    // setSelectedId(id)
+    console.log(selectedId?.id, 'delete')
     setIsModalOpenDelete(true)
+    setAnchorEl(null)
   }
   const confirmDeleteAction = async () => {
     try {
       setIsModalOpenDelete(false)
-      const res = await deleteMediaFile(selectedId)
+      const res = await deleteMediaFile(selectedId?.id)
       if (res?.success) {
         Toaster({ type: 'success', message: res?.message })
         await getMediaListUserId(userId)
@@ -137,40 +129,42 @@ const Media = () => {
       console.error('Error uploading files:', error)
     }
   }
-
-  const getFileIcon = fileName => {
-    const fileExtension = fileName.split('.').pop().toLowerCase()
-    switch (fileExtension) {
-      case 'pdf':
-        return <PictureAsPdfIcon />
-      case 'doc':
-      case 'docx':
-        return <DescriptionIcon />
-      case 'xls':
-      case 'xlsx':
-        return <InsertDriveFileIcon />
-      case 'jpg':
-      case 'jpeg':
-      case 'png':
-      case 'gif':
-        return <ImageIcon />
-      default:
-        return <InsertDriveFileIcon />
+  const handleDownload = async id => {
+    console.log('filePreviews', filePreviews)
+    if (selectedId) {
+      const link = document.createElement('a')
+      link.href = selectedId.user_media
+      link.download = selectedId.file_original_name
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+      Toaster({ type: 'success', message: 'File downloaded successfully!' })
+      setAnchorEl(null)
+    } else {
+      Toaster({ type: 'error', message: 'No file selected for download.' })
+      setAnchorEl(null)
     }
   }
+
+  console.log(imgPath, 'auth')
+
   const getIconByFileType = fileName => {
     const extension = fileName.split('.').pop().toLowerCase()
     switch (extension) {
       case 'pdf':
-        return pdfIcon
+        return { icon: imgPath?.pdf?.image_path, bgColor: imgPath?.pdf?.bg_color }
       case 'xls':
       case 'xlsx':
-        return xlsIcon
+        return { icon: imgPath?.xls?.image_path, bgColor: imgPath?.xls?.bg_color }
       case 'doc':
       case 'docx':
-        return docIcon
+        return { icon: imgPath?.document?.image_path, bgColor: imgPath?.document?.bg_color }
+      case 'mp3':
+      case 'wav':
+      case 'ogg':
+        return { icon: imgPath?.audio?.image_path, bgColor: imgPath?.audio?.bg_color }
       default:
-        return '' // default icon if the file type is unknown
+        return { icon: imgPath?.default?.image_path, bgColor: imgPath?.default?.bg_color }
     }
   }
 
@@ -211,12 +205,10 @@ const Media = () => {
     }
   }
 
-  const [anchorEl, setAnchorEl] = useState(null)
-
-  const handleClick = (event, id) => {
+  const handleClick = (event, media) => {
     setAnchorEl(event.currentTarget)
-    setSelectedId(id)
-    console.log(id)
+    setSelectedId(media)
+    console.log(media)
   }
 
   const handleClose = () => {
@@ -302,7 +294,7 @@ const Media = () => {
                 <Typography
                   variant='subtitle1'
                   gutterBottom
-                  sx={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontWeight: 'bold' }}
+                  sx={{ display: 'flex', alignItems: 'center', margin: '16px 0', fontWeight: 'bold' }}
                 >
                   <Divider sx={{ width: 30, marginRight: '5px' }} orientation='horizontal' />
                   <EventIcon sx={{ marginRight: '5px' }} />
@@ -315,21 +307,23 @@ const Media = () => {
                     <React.Fragment key={mediaIndex}>
                       <Grid item xs={12} sm={6} md={4} lg={3}>
                         <Card sx={{ position: 'relative', height: '100%', bgcolor: '#f2f2f2' }}>
-                          <CardContent sx={{ display: 'flex', alignItems: 'center', pb: 7 }}>
-                            <Typography
-                              variant='subtitle2'
-                              gutterBottom
-                              sx={{
-                                ml: 2,
-                                mb: 0,
-                                whiteSpace: 'nowrap',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                maxWidth: 180 // Adjust this based on your design
-                              }}
-                            >
-                              {media?.file_original_name}
-                            </Typography>
+                          <CardContent sx={{ display: 'flex', alignItems: 'center', pb: 1 }}>
+                            <Tooltip title={media?.file_original_name} arrow>
+                              <Typography
+                                variant='subtitle2'
+                                gutterBottom
+                                sx={{
+                                  ml: 2,
+                                  mb: 0,
+                                  whiteSpace: 'nowrap',
+                                  overflow: 'hidden',
+                                  textOverflow: 'ellipsis',
+                                  maxWidth: 180 // Adjust this based on your design
+                                }}
+                              >
+                                {media?.file_original_name}
+                              </Typography>
+                            </Tooltip>
                           </CardContent>
 
                           {media?.user_media && (
@@ -340,7 +334,7 @@ const Media = () => {
                                   height='160'
                                   image={media?.user_media}
                                   alt={media?.file_original_name}
-                                  sx={{ objectFit: 'cover', borderRadius: 2, p: 3 }}
+                                  sx={{ objectFit: 'cover', borderRadius: 2.6, p: 5 }}
                                 />
                               ) : (
                                 <Box
@@ -348,11 +342,14 @@ const Media = () => {
                                     display: 'flex',
                                     justifyContent: 'center',
                                     alignItems: 'center',
-                                    height: '160px'
+                                    height: 120,
+                                    borderRadius: 1,
+                                    bgcolor: getIconByFileType(media?.file_original_name)?.bgColor,
+                                    m: 5
                                   }}
                                 >
                                   <Image
-                                    src={getIconByFileType(media?.file_original_name)}
+                                    src={getIconByFileType(media?.file_original_name)?.icon}
                                     alt=''
                                     width={80}
                                     height={80}
@@ -369,34 +366,18 @@ const Media = () => {
                             sx={{
                               position: 'absolute',
                               top: 12,
-                              right: 8,
+                              right: 1,
                               cursor: 'pointer'
                             }}
-                            onClick={e => handleClick(e, media?.id)}
+                            onClick={e => handleClick(e, media)}
                           >
                             <Icon icon='mdi:dots-vertical' />
                           </IconButton>
 
-                          <CardContent sx={{ display: 'flex', alignItems: 'center', pb: 1 }}>
-                            <Tooltip title={media?.file_original_name} arrow>
-                              <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                {getFileIcon(media?.file_original_name)}
-                                <Typography
-                                  variant='subtitle2'
-                                  gutterBottom
-                                  sx={{
-                                    ml: 2,
-                                    mb: 0,
-                                    whiteSpace: 'nowrap',
-                                    overflow: 'hidden',
-                                    textOverflow: 'ellipsis',
-                                    maxWidth: 200 // Adjust this based on your design
-                                  }}
-                                >
-                                  {media?.file_original_name}
-                                </Typography>
-                              </Box>
-                            </Tooltip>
+                          <CardContent
+                            sx={{ display: 'flex', alignItems: 'center', justifyContent: 'end', pb: 0, pt: 0 }}
+                          >
+                            <Box>{moment(media?.file_original_name?.created_at).format('hh:mm A')}</Box>
                           </CardContent>
                         </Card>
                       </Grid>
@@ -410,8 +391,8 @@ const Media = () => {
       </CardContent>
 
       <Menu keepMounted id='long-menu' anchorEl={anchorEl} onClose={handleClose} open={Boolean(anchorEl)}>
-        <MenuItem onClick={e => onHandleDropdown(e)}>View</MenuItem>
-        <MenuItem>Download</MenuItem>
+        <MenuItem>View</MenuItem>
+        <MenuItem onClick={handleDownload}>Download</MenuItem>
         <MenuItem onClick={handleDelete}>Delete</MenuItem>
       </Menu>
 
