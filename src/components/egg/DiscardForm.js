e640517a3a60edfe5e2ec170649efd7de3dc6c13
Nihal kishore <nihal.desuntechnology@gmail.com>
2024-07-16 15:13:37 +0530

    Discard Ui Change

--- src/components/egg/DiscardForm.js
+++ src/components/egg/DiscardForm.js
@@ -240,7 +240,19 @@ const DiscardForm = ({ isOpen, setIsOpen, eggID, callApi }) => {
           </Box>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Card sx={{ p: 4, m: 4, display: 'flex', flexDirection: 'column', gap: 3 }}>
+            <Box
+              sx={{
+                p: 4,
+                m: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 3,
+                bgcolor: '#fff',
+                borderRadius: '8px',
+                border: 1,
+                borderColor: '#c3cec7'
+              }}
+            >
               {discardReason?.map(item => (
                 <Box
                   key={item?.id}
@@ -270,14 +282,26 @@ const DiscardForm = ({ isOpen, setIsOpen, eggID, callApi }) => {
                   </FormControl>
                 </Box>
               ))}
-            </Card>
+            </Box>
             {!reason && <FormHelperText sx={{ color: 'error.main', m: 5 }}>State Is Required</FormHelperText>}
 
             <Typography variant='h6' sx={{ m: 5 }}>
               Add Reason For Discard
             </Typography>
 
-            <Card sx={{ p: 4, m: 4, display: 'flex', flexDirection: 'column', gap: 3 }}>
+            <Box
+              sx={{
+                p: 4,
+                m: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 3,
+                bgcolor: '#fff',
+                borderRadius: '8px',
+                border: 1,
+                borderColor: '#c3cec7'
+              }}
+            >
               <FormControl fullWidth>
                 <Controller
                   name='comment'
@@ -376,9 +400,22 @@ const DiscardForm = ({ isOpen, setIsOpen, eggID, callApi }) => {
                   )}
                 </Grid>
               </Grid>
-            </Card>
+            </Box>
 
-            <Card sx={{ p: 4, m: 4, display: 'flex', flexDirection: 'column', gap: 3, mb: 35 }}>
+            <Box
+              sx={{
+                p: 4,
+                m: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 3,
+                mb: 35,
+                bgcolor: '#fff',
+                borderRadius: '8px',
+                border: 1,
+                borderColor: '#c3cec7'
+              }}
+            >
               <Typography variant='h6'>Necropsy Needed ?</Typography>
 
               <Stack>
@@ -441,7 +478,7 @@ const DiscardForm = ({ isOpen, setIsOpen, eggID, callApi }) => {
                   ></Controller>
                 </FormControl>
               </Stack>
-            </Card>
+            </Box>
             <Box
               sx={{
                 height: '122px',
