58b03538772af8a70b9fbf40fa9a55ec3ab512da
clear <anilrathod@lifesciencetrust.com>
2024-11-06 18:18:47 +0530

    Stock Report Details ui design

---
+++ src/views/pages/pharmacy/stock/stockReportDetails.js
@@ -0,0 +1,222 @@
+// ** React Imports
+import { useState, useEffect } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { CircularProgress, Accordion, AccordionSummary, AccordionDetails, ListItemText, ListItem } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Button from '@mui/material/Button'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Next.js Imports
+import { useRouter } from 'next/router'
+
+const StockReportDetails = props => {
+  // ** Props
+  const {
+    addEventSidebarOpen,
+    handleSidebarClose,
+    handleSubmitData,
+    resetForm,
+    submitLoader,
+    editParams,
+    purchaseByStockIdList
+  } = props
+
+  const [loading, setLoading] = useState(true)
+  const [expanded, setExpanded] = useState(false)
+  const router = useRouter()
+
+  const handleAccordionChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
+  }
+
+  useEffect(() => {
+    if (purchaseByStockIdList && purchaseByStockIdList.length > 0) {
+      setLoading(false)
+    } else {
+      setLoading(true)
+    }
+  }, [purchaseByStockIdList])
+
+  const handleNavigate = productId => {
+    router.push(`pharmacy/purchase/add-purchase/?id=${productId}&action=edit`)
+  }
+
+  return (
+    <Drawer
+      anchor='right'
+      open={addEventSidebarOpen}
+      ModalProps={{ keepMounted: true }}
+      sx={{ '& .MuiDrawer-paper': { width: ['100%', 400] } }}
+    >
+      <Box
+        className='sidebar-header'
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          backgroundColor: 'background.default',
+          p: theme => theme.spacing(3, 3.255, 3, 5.255)
+        }}
+      >
+        <Typography variant='h6'>Stock Report Lists</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton size='small' onClick={handleSidebarClose} sx={{ color: 'text.primary' }}>
+            <Icon icon='mdi:close' fontSize={20} />
+          </IconButton>
+        </Box>
+      </Box>
+
+      <Box className='sidebar-body' sx={{ p: theme => theme.spacing(5, 6) }}>
+        {loading ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CircularProgress />
+          </Box>
+        ) : (
+          purchaseByStockIdList.map((product, idx) => (
+            <Box key={idx} mb={6}>
+              <Accordion expanded={expanded === `panel${idx}`} onChange={handleAccordionChange(`panel${idx}`)}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`panel${idx}-content`}
+                  id={`panel${idx}-header`}
+                >
+                  <Typography>{product.stock_name}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {product.product_details.map((detail, index) => (
+                    <ListItem key={index}>
+                      <ListItemText
+                        primary={`Batch No: ${detail.batch_no}`}
+                        secondary={
+                          <Box>
+                            <Typography variant='body2'>Net Amount: {detail.net_amount}</Typography>
+                            <Typography variant='body2'>Unit Price: {detail.unit_price}</Typography>
+                            <Typography variant='body2'>Quantity: {detail.qty}</Typography>
+                          </Box>
+                        }
+                      />
+                    </ListItem>
+                  ))}
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={() => handleNavigate(product.id)}
+                    sx={{ float: 'right', mb: 4 }}
+                  >
+                    update product price
+                  </Button>
+                </AccordionDetails>
+              </Accordion>
+            </Box>
+          ))
+        )}
+      </Box>
+    </Drawer>
+  )
+}
+
+export default StockReportDetails
+
+// // ** React Imports
+// import { useState, useEffect, useCallback, Fragment } from 'react'
+
+// // ** MUI Imports
+// import Box from '@mui/material/Box'
+// import Drawer from '@mui/material/Drawer'
+
+// import IconButton from '@mui/material/IconButton'
+// import Typography from '@mui/material/Typography'
+
+// import { LoadingButton } from '@mui/lab'
+
+// // ** Third Party Imports
+
+// // ** Icon Imports
+// import Icon from 'src/@core/components/icon'
+// import { CircularProgress, List, ListItem, ListItemText, ListSubheader } from '@mui/material'
+
+// const StockReportDetails = props => {
+//   // ** Props
+//   const {
+//     addEventSidebarOpen,
+//     handleSidebarClose,
+//     handleSubmitData,
+//     resetForm,
+//     submitLoader,
+//     editParams,
+//     purchaseByStockIdList
+//   } = props
+
+//   console.log(purchaseByStockIdList, 'purchaseByStockIdList')
+//   const [loading, setLoading] = useState(true)
+
+//   useEffect(() => {
+//     if (purchaseByStockIdList && purchaseByStockIdList.length > 0) {
+//       setLoading(false) // Data is available, hide the loader
+//     } else {
+//       setLoading(true) // Show the loader if the list is empty or being fetched
+//     }
+//   }, [purchaseByStockIdList])
+
+//   return (
+//     <Drawer
+//       anchor='right'
+//       open={addEventSidebarOpen}
+//       ModalProps={{ keepMounted: true }}
+//       sx={{ '& .MuiDrawer-paper': { width: ['100%', 400] } }}
+//     >
+//       <Box
+//         className='sidebar-header'
+//         sx={{
+//           display: 'flex',
+//           justifyContent: 'space-between',
+//           backgroundColor: 'background.default',
+//           p: theme => theme.spacing(3, 3.255, 3, 5.255)
+//         }}
+//       >
+//         <Typography variant='h6'> Stock Report Lists</Typography>
+//         <Box sx={{ display: 'flex', alignItems: 'center' }}>
+//           <IconButton size='small' onClick={handleSidebarClose} sx={{ color: 'text.primary' }}>
+//             <Icon icon='mdi:close' fontSize={20} />
+//           </IconButton>
+//         </Box>
+//       </Box>
+//       <Box className='sidebar-body' sx={{ p: theme => theme.spacing(5, 6) }}>
+//         {loading ? (
+//           <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+//             <CircularProgress />
+//           </Box>
+//         ) : (
+//           purchaseByStockIdList.map((product, idx) => (
+//             <Box key={idx} mb={3}>
+//               <List subheader={<Box>{product.stock_name}</Box>}>
+//                 {product.product_details.map((detail, index) => (
+//                   <ListItem key={index}>
+//                     <ListItemText
+//                       primary={`Batch No: ${detail.batch_no}`}
+//                       secondary={
+//                         <Box>
+//                           <Typography variant='body2'>Net Amount: {detail.net_amount}</Typography>
+//                           <Typography variant='body2'>Unit Price: {detail.unit_price}</Typography>
+//                           <Typography variant='body2'>Quantity: {detail.qty}</Typography>
+//                         </Box>
+//                       }
+//                     />
+//                   </ListItem>
+//                 ))}
+//               </List>
+//             </Box>
+//           ))
+//         )}
+//       </Box>
+//     </Drawer>
+//   )
+// }
+
+// export default StockReportDetails
