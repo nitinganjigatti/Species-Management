0c487f11bf57e07b81dc8cd70296c842c8e310cf
Naseer <naseer@trellisys.net>
2025-01-01 17:51:24 +0530

    Fixed filters are search alignment issues.

--- src/views/table/data-grid/CommonTable.js
+++ src/views/table/data-grid/CommonTable.js
@@ -37,7 +37,7 @@ const CommonTable = ({
           overflowX: 'auto'
         },
         '.MuiDataGrid-main': {
-          margin: '2px',
+          // margin: '2px',
           borderLeft: '1px solid #0000000D',
           borderRight: '1px solid #0000000D',
 
