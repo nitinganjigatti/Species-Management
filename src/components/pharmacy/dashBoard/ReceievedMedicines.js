7c27c49f7c2e31f352082e17ee44dda13cc10154
Naseer <naseer@trellisys.net>
2025-01-27 17:39:18 +0530

    Bug fixes.

--- src/components/pharmacy/dashBoard/ReceievedMedicines.js
+++ src/components/pharmacy/dashBoard/ReceievedMedicines.js
@@ -1,28 +1,15 @@
-// import Card from '@mui/material/Card'
-// import { useTheme } from '@mui/material/styles'
-// import CardHeader from '@mui/material/CardHeader'
-// import Typography from '@mui/material/Typography'
-// import CardContent from '@mui/material/CardContent'
-// import { useEffect, useState } from 'react'
-// import ReactApexcharts from 'src/@core/components/react-apexcharts'
-// import { Button, Checkbox, FormControlLabel, Box } from '@mui/material'
-// import Router from 'next/router'
-// import { usePharmacyContext } from 'src/context/PharmacyContext'
-// import { getReceivedMedicineschart } from 'src/lib/api/pharmacy/dashboard'
-
 import React, { useEffect, useState } from 'react'
 import { getReceivedMedicineschart } from 'src/lib/api/pharmacy/dashboard'
 import MonthlyChart from 'src/views/utility/monthlychart'
 
 const ReceivedMedicines = () => {
-  const [dispatchData, setDispatchData] = useState({ dispatch_count: [], dispatch_value: [] })
+  const [purchaseData, setPurchaseData] = useState({ purchase_count: [], purchase_value: [] })
 
-  const fetchDispatchData = async () => {
+  const fetchPurchaseData = async () => {
     try {
       const result = await getReceivedMedicineschart()
       if (result?.success === true && result?.data) {
-        debugger
-        setDispatchData(result.data)
+        setPurchaseData(result.data)
       }
     } catch (error) {
       console.error(error)
@@ -30,25 +17,19 @@ const ReceivedMedicines = () => {
   }
 
   useEffect(() => {
-    fetchDispatchData()
+    fetchPurchaseData()
   }, [])
-
-  const handleClick = () => {
-    Router.push({
-      pathname: '/pharmacy/reports/received-medicines-report'
-    })
-  }
 
   return (
     <>
       <MonthlyChart
         title='Received Medicines'
-        data={dispatchData}
+        data={purchaseData}
         barColor='#FA6140'
-        lineColor='#FFBDA8'
+        lineColor='#fa614059'
         barName='Received Value'
         lineName='Received Count'
-        viewMorePath='/pharmacy/reports/received-medicines-report'
+        viewMorePath='/pharmacy/reports/month-wise-purchase'
       />
     </>
   )
