f4dc97a6bcd92a05b883c0f78b457221f0b2d359
clear <anilrathod@lifesciencetrust.com>
2025-02-25 18:54:07 +0530

    formatAmountToReadableDigit and new-product-request  query  params added

--- src/views/pages/pharmacy/requests-by-stores/product-details/index.js
+++ src/views/pages/pharmacy/requests-by-stores/product-details/index.js
@@ -465,29 +465,31 @@ const RequestedProductDetails = props => {
                           } `
                         : 'NA'}
                     </Typography>
-                    <Typography
-                      sx={{
-                        color: theme.palette.customColors.OnSurfaceVariant,
-                        fontSize: '12px',
-                        fontWeight: 400,
-                        fontFamily: 'Inter'
-                      }}
-                    >
-                      Available Quantity:
+                    {selectedPharmacy.type === 'central' && (
                       <Typography
-                        component='span'
                         sx={{
-                          color: theme.palette.customColors.neutralPrimary,
-                          fontSize: '14px',
+                          color: theme.palette.customColors.OnSurfaceVariant,
+                          fontSize: '12px',
                           fontWeight: 400,
                           fontFamily: 'Inter'
                         }}
                       >
-                        {requestedProducts?.total_available_quantity
-                          ? requestedProducts?.total_available_quantity
-                          : 'NA'}
+                        Available Quantity:
+                        <Typography
+                          component='span'
+                          sx={{
+                            color: theme.palette.customColors.neutralPrimary,
+                            fontSize: '14px',
+                            fontWeight: 400,
+                            fontFamily: 'Inter'
+                          }}
+                        >
+                          {requestedProducts?.total_available_quantity
+                            ? requestedProducts?.total_available_quantity
+                            : 'NA'}
+                        </Typography>
                       </Typography>
-                    </Typography>
+                    )}
                   </Box>
                 </Grid>
                 <Grid item xs={1} sm={1} sx={{ float: 'right', textAlign: 'right', height: 'auto' }}>
