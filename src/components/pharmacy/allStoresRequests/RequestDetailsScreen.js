892b7af7505f35ffdd149959441ca27d5a4a05bd
clear <anilrathod@lifesciencetrust.com>
2025-04-28 11:36:54 +0530

    changes

--- src/components/pharmacy/allStoresRequests/RequestDetailsScreen.js
+++ src/components/pharmacy/allStoresRequests/RequestDetailsScreen.js
@@ -37,7 +37,7 @@ const RequestDetailsScreen = () => {
 
   useEffect(() => {
     if (detailsTab !== router.query.mainTab) {
-      debugger
+      // debugger
       updateUrlParams({
         mainTab: detailsTab
       })
