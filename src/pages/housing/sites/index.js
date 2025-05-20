e2de593c628945d11b647a7532d1b55fb8113dd8
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2025-05-20 15:52:27 +0530

    FEAT: Insights card components implementation

--- src/pages/housing/sites/index.js
+++ src/pages/housing/sites/index.js
@@ -1,6 +1,33 @@
+import { Box, Breadcrumbs, Card, CardHeader, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+import InsightsCard from 'src/views/utility/insights/InsightsCard'
+
 const Sites = () => {
-  return <>
-  Sites page
-  </>
+  const router = useRouter()
+
+  const handleHousingClick = () => {
+    // router.push('/housing')
+  }
+
+  return (
+    <Box>
+      <Breadcrumbs aria-label='breadcrumb' sx={{ mb: 5 }}>
+        <Typography color='inherit' sx={{ cursor: 'pointer' }} onClick={handleHousingClick}>
+          Housing
+        </Typography>
+
+        <Typography sx={{ cursor: 'pointer' }} color='text.primary'>
+          Site List
+        </Typography>
+      </Breadcrumbs>
+      <Box>
+        <InsightsCard />
+      </Box>
+      <Card>
+        <Box>Content</Box>
+      </Card>
+    </Box>
+  )
 }
+
 export default Sites
