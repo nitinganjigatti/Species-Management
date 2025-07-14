6bf4194f245eb0a93c5ac5c1fac37a477db94619
Sandeep <sandeep.desuntech@gmail.com>
2025-07-14 12:01:31 +0530

    date related changes

--- src/pages/report/animalAssessment/index.js
+++ src/pages/report/animalAssessment/index.js
@@ -188,7 +188,11 @@ const AnimalAssessment = () => {
             // Number(assessment?.assessment_value) > 1 && assessment?.uom_abbr ? 's' : ''
             ''
           }`,
-          date: moment(Utility.convertUTCToLocalDate(assessment.assessment_recorded_date)).format('DD MMMM YYYY'),
+          date: moment(
+            Utility.convertUTCToLocalDate(
+              assessment.assessment_recorded_date + ' ' + assessment.assessment_recorded_time
+            )
+          ).format('DD MMMM YYYY'),
           time: Utility.extractHoursAndMinutes(
             Utility?.convertUTCToLocal(assessment.assessment_recorded_date + ' ' + assessment.assessment_recorded_time)
           ),
