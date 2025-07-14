85ffe3f26abe4a4a952649f9eada96c15d4a52ae
Sandeep <sandeep.desuntech@gmail.com>
2025-07-14 16:01:54 +0530

    added dynamic height based on height of row by dom in sticky table

--- src/views/table/sticky-table.js
+++ src/views/table/sticky-table.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {
   Table,
   TableHead,
@@ -53,6 +53,8 @@ const StickyTable = ({
   modifyColumnPinning = false
 }) => {
   const theme = useTheme()
+  const rowRefs = useRef([])
+
   const [defaultRowsInView, setDefaultRowsInView] = useState(rowsInView)
   const [defaultRowsInViewOption, setDefaultRowsInViewOption] = useState(rowsInViewOptions)
   const [rowPerPageCount, setRowPerPageCount] = useState(paginationModel?.pageSize || 10)
@@ -69,6 +71,20 @@ const StickyTable = ({
   const tableTotalHeight = defaultRowsInView * rowHeight + headerHeight + (hasSubHeader ? subHeaderHeight : 0)
 
   // const tableTotalHeight = defaultRowsInView * rowHeight + headerHeight + subHeaderHeight
+
+  const [dynamicTableHeight, setDynamicTableHeight] = useState(
+    defaultRowsInView * rowHeight + headerHeight + (hasSubHeader ? subHeaderHeight : 0)
+  )
+
+  useEffect(() => {
+    if (filteredRows.length > 0) {
+      const totalVisibleHeight = rowRefs.current
+        .slice(0, defaultRowsInView)
+        .reduce((sum, ref) => sum + (ref?.offsetHeight || rowHeight), 0)
+
+      setDynamicTableHeight(totalVisibleHeight + headerHeight + (hasSubHeader ? subHeaderHeight : 0))
+    }
+  }, [filteredRows, defaultRowsInView])
 
   useEffect(() => {
     const leftPinnedColumns = columns.filter(col => col.pinned === 'left')
@@ -91,6 +107,10 @@ const StickyTable = ({
       setFilteredRows(rows)
     }
   }, [rows])
+
+  useEffect(() => {
+    rowRefs.current = []
+  }, [filteredRows])
 
   useEffect(() => {
     if (searchMode === 'server') {
@@ -677,6 +697,7 @@ const StickyTable = ({
 
       return (
         <TableRow
+          ref={el => (rowRefs.current[rowIndex] = el)}
           key={rowIndex}
           onClick={e => {
             if (onRowClick) {
@@ -1015,15 +1036,8 @@ const StickyTable = ({
           component={Paper}
           sx={{
             borderRadius: 2,
-            // height: rowSelection
-            //   ? Math.max(5 * (rowHeight + 0.88), defaultRowsInView * (rowHeight + 0.88)) +
-            //     headerHeight +
-            //     subHeaderHeight +
-            //     16
-            //   : Math.max(5 * (rowHeight + 0.8), defaultRowsInView * (rowHeight + -0.1)) +
-            //     headerHeight +
-            //     subHeaderHeight,
-            height: tableTotalHeight,
+            // height: tableTotalHeight,
+            height: dynamicTableHeight,
             // maxHeight: tableTotalHeight,
             overflowY: 'auto',
             position: 'relative',
