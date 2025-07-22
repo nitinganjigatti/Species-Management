7757f8a6a19869e74906e9469a3e7f31512a3675
sandeep mandal <mandalsandeep670@gmail.com>
2025-07-23 01:47:00 +0530

    refactored code for species card

--- src/views/utility/SpeciesCard.js
+++ src/views/utility/SpeciesCard.js
@@ -17,10 +17,11 @@ function SpeciesCard({ species }) {
     setImgSrc('/images/housing/species-icon-colored.svg')
   }
 
+
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
 
-      {loading ? <Skeleton variant='circular' width={40} height={40} animation='wave' /> :
+      {/* {loading ? <Skeleton variant='circular' width={40} height={40} animation='wave' /> :
 
         <Avatar
           sx={{
@@ -47,8 +48,76 @@ function SpeciesCard({ species }) {
             }
           }}
         />}
-      <Box>
 
+        <Tooltip title={species.common_name}>
+          <Typography
+            sx={{
+              color: theme.palette.customColors.OnSurfaceVariant,
+              fontSize: '16px',
+              fontWeight: 600,
+              display: '-webkit-box',
+              WebkitLineClamp: 1,
+              WebkitBoxOrient: 'vertical',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis'
+            }}
+          >
+            {species.common_name ? species.common_name : '-'}
+          </Typography>
+        </Tooltip>
+        <Tooltip
+          title={
+            species.scientific_name ? species.scientific_name : species.complete_name ? species.complete_name : '-'
+          }
+        >
+          <Typography
+            sx={{
+              color: theme.palette.customColors.OnSurfaceVariant,
+              fontSize: '16px',
+              fontWeight: 400,
+              fontStyle: 'italic',
+              display: '-webkit-box',
+              WebkitLineClamp: 1,
+              WebkitBoxOrient: 'vertical',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis'
+            }}
+          >
+            {species.scientific_name ? species.scientific_name : species.complete_name ? species.complete_name : '-'}
+          </Typography>
+        </Tooltip> */}
+
+      {species?.default_icon && (
+        <Box sx={{ position: 'relative', width: 40, height: 40 }}>
+          {loading && <Skeleton variant='circular' width={40} height={40} animation='wave' />}
+
+          <Avatar
+            sx={{
+              width: 40,
+              height: 40,
+              '& img': {
+                objectFit: 'inherit'
+              },
+              borderRadius:
+                species?.default_icon && species.default_icon.includes('.svg')
+                  ? '50%'
+                  : species?.default_icon
+                    ? '50%'
+                    : 'unset'
+            }}
+            src={imgSrc}
+            alt={species.scientific_name}
+            slotProps={{
+              img: {
+                onLoad: handleImageLoad,
+                onError: handleImageError
+              }
+            }}
+          />
+        </Box>
+      )}
+
+      <Box>
         {(species.primary_identifier_type && species.primary_identifier_value) &&
           <Tooltip title={`${species.primary_identifier_type}: ${species.primary_identifier_value}`}><Typography
             sx={{
@@ -108,3 +177,5 @@ function SpeciesCard({ species }) {
 }
 
 export default SpeciesCard
+
+
