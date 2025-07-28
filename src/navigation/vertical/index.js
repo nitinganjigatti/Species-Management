ad3022a8cff5b0bee9ae313f27bd56fa0a334cb1
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2025-07-28 14:44:20 +0530

    FEAT: Module level access integration

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -36,6 +36,8 @@ const ComposeNavigation = () => {
 
   const pariveshAccess = authData?.userData?.roles?.settings?.enable_parivesh
 
+  const housingModule = authData?.userData?.roles?.settings?.enable_housing_in_web
+
   // console.log('labList', labList)
   const { selectedPharmacy } = usePharmacyContext()
 
@@ -81,8 +83,10 @@ const ComposeNavigation = () => {
     navigationArray.push(...pariveshNav)
   }
 
-  const housingnav = housingNavigation()
-  navigationArray.push(...housingnav)
+  if (housingModule) {
+    const housingNav = housingNavigation()
+    navigationArray.push(...housingNav)
+  }
 
   const medicalNav = medicalNavigation({
     userSettings
