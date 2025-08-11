80ca93c555676528805cb8526b7d1ab8c6d9eb0e
irayyaTrellisys <128675054+irayyaTrellisys@users.noreply.github.com>
2025-08-11 14:12:46 +0530

    Replaced usage of RenderUtility.renderUserAvatarDetails with a new UserAvatarDetails component in the pharmacy purchase list. Added the UserAvatarDetails component to provide a reusable and customizable way to display user avatars, names, and dates.
    Replaced usage of RenderUtility.renderUserAvatarDetails with a new UserAvatarDetails component in the pharmacy purchase list. Added the UserAvatarDetails component to provide a reusable and customizable way to display user avatars, names, and dates.

---
+++ src/views/utility/UserAvatarDetails.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import { Typography, Box, Tooltip } from '@mui/material'
+import CustomAvatar from 'src/@core/components/mui/avatar'
+import Utility from 'src/utility'
+
+function UserAvatarDetails({
+  profile_image,
+  user_name,
+  date,
+  text_color,
+  description,
+  size = 'large',
+  show_time = false
+}) {
+  const avatarSizes = {
+    small: {
+      profile_picture: { width: '24px', height: '24px' },
+      gap: '8px',
+      user_name: { fontSize: '12px', fontWeight: 500 },
+      date: { fontSize: '10px', fontWeight: 500 }
+    },
+    medium: {
+      profile_picture: { width: '32px', height: '32px' },
+      gap: '8px',
+      user_name: { fontSize: '14px', fontWeight: 500 },
+      date: { fontSize: '12px', fontWeight: 400, lineHeight: '14px' }
+    },
+    large: {
+      profile_picture: { width: '40px', height: '40px' },
+      gap: '12px',
+      user_name: { fontSize: '14px', fontWeight: 500 },
+      date: { fontSize: '12px', fontWeight: 400, lineHeight: '21px' }
+    }
+  }
+  const selectedAvatarSize = avatarSizes[size] || avatarSizes[size]
+
+  return (
+    <>
+      {user_name ? (
+        <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'default' }}>
+          {profile_image ? (
+            <CustomAvatar
+              src={profile_image}
+              sx={{
+                mr: avatarSizes[size].gap,
+                ...(selectedAvatarSize?.profile_picture || {})
+              }}
+            />
+          ) : (
+            <CustomAvatar
+              sx={{
+                mr: avatarSizes[size].gap,
+                ...(selectedAvatarSize?.profile_picture || {})
+              }}
+            ></CustomAvatar>
+          )}
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            {user_name && (
+              <>
+                <Tooltip title={user_name}>
+                  <Typography
+                    variant='subtitle2'
+                    sx={{
+                      color: text_color ?? 'text.primary',
+
+                      // fontSize: fontSize,
+                      whiteSpace: 'nowrap',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      maxWidth: 100,
+                      ...(selectedAvatarSize?.user_name || {})
+                    }}
+
+                    // component={'span'}
+                  >
+                    {user_name ? user_name : 'NA'}
+                  </Typography>
+                </Tooltip>
+
+                {description && (
+                  <>
+                    <Tooltip title={description}>
+                      <Typography sx={{ color: theme => text_color ?? theme.palette.common.white }} variant='body2'>
+                        {description}
+                      </Typography>
+                    </Tooltip>
+                  </>
+                )}
+              </>
+            )}
+
+            {date && (
+              <Typography variant='caption' sx={{ lineHeight: 1.6667, ...(selectedAvatarSize?.date || {}) }}>
+                <span>
+                  {show_time ? (
+                    <>
+                      {Utility.convertUTCToLocaltime(date)}
+                      <span> &bull; </span>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                </span>
+                <span>{date ? Utility.convertUtcToLocalReadableDate(date) : ''}</span>
+              </Typography>
+            )}
+          </Box>
+        </Box>
+      ) : (
+        <Typography variant='subtitle2' sx={{ color: 'text.primary', ml: 3, cursor: 'default' }}>
+          -
+        </Typography>
+      )}
+    </>
+  )
+}
+
+export default React.memo(UserAvatarDetails)
