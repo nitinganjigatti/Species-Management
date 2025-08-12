5f3adfc10cd4cb2dbae069c0535d37005b7e418d
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2025-08-12 16:44:07 +0530

    FEAT: Creation of clinical assessment and symptoms

---
+++ src/views/pages/hospital/inpatient/ClinicalAssessment.js
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const ClinicalAssessment = () => {
+  return (
+    <Box>ClinicalAssessment</Box>
+  )
+}
+
+export default ClinicalAssessment
