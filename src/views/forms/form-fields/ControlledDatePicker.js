869b6ef2dfc7b27a2547158eb279ad12d041bdea
Irayya <128675054+irayyaTrellisys@users.noreply.github.com>
2025-08-29 12:07:12 +0530

    UPDATE: Add prescription scheduling and medicine selection UI
    Introduces AddMedicineToPrescription component and a new page for scheduling prescriptions. Updates PrescriptionLayout and PrescriptionMonitoringGrid to support medication selection, time slot management, and improved UI for prescription monitoring. Enhances form field components for better control and flexibility.

--- src/views/forms/form-fields/ControlledDatePicker.js
+++ src/views/forms/form-fields/ControlledDatePicker.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import { Controller } from 'react-hook-form'
 import MUIDatePicker from './MUIDatePicker'
-import { size } from 'lodash'
 
 const ControlledDatePicker = ({
   name,
