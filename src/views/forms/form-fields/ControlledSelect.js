efc113710c70c1cb3c4fc59ed12a1869ee6a8a3b
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2025-09-29 17:28:13 +0530

    UPDATE: Enhance ControlledTextField components with improved error handling and helper text display

--- src/views/forms/form-fields/ControlledSelect.js
+++ src/views/forms/form-fields/ControlledSelect.js
@@ -65,3 +65,75 @@ const ControlledSelect = ({
 }
 
 export default React.memo(ControlledSelect)
+
+// Updated code
+
+// import React from 'react'
+// import { Controller } from 'react-hook-form'
+// import { FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material'
+
+// const ControlledSelect = ({
+//   name,
+//   label,
+//   control,
+//   errors,
+//   options = [],
+//   required = false,
+//   fullWidth = true,
+//   size = 'large',
+//   getOptionLabel = option => option,
+//   getOptionValue = option => option,
+//   isOptionDisabled = () => false,
+//   onChangeExtra = () => {},
+//   disabled = false,
+//   sx = {}
+// }) => {
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       rules={{ required }}
+//       render={({ field, fieldState }) => {
+//         const error = fieldState.error
+//         const helperText = error?.message || ''
+
+//         return (
+//           <FormControl size={size} fullWidth={fullWidth} error={Boolean(error)}>
+//             <InputLabel
+//               id={`${name}-label`}
+//               sx={{
+//                 color: error ? 'error.main' : 'text.primary',
+//                 '&.Mui-focused': {
+//                   color: error ? 'error.main' : 'primary.main'
+//                 }
+//               }}
+//             >
+//               {label}
+//             </InputLabel>
+//             <Select
+//               {...field}
+//               labelId={`${name}-label`}
+//               label={label}
+//               disabled={disabled}
+//               error={Boolean(error)}
+//               sx={sx}
+//               onChange={e => {
+//                 field.onChange(e)
+//                 onChangeExtra(e)
+//               }}
+//             >
+//               {options.map((option, index) => (
+//                 <MenuItem key={index} value={getOptionValue(option)} disabled={isOptionDisabled(option)}>
+//                   {getOptionLabel(option)}
+//                 </MenuItem>
+//               ))}
+//             </Select>
+//             {helperText && <FormHelperText sx={{ ml: '14px' }}>{helperText}</FormHelperText>}
+//           </FormControl>
+//         )
+//       }}
+//     />
+//   )
+// }
+
+// export default React.memo(ControlledSelect)
