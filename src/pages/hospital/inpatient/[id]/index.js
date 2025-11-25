05b74686922b1a5dd658346de6e973ac7b9fe67e
mahesh-sangeet <mahesh@lifesciencetrust.com>
2025-11-25 14:45:47 +0530

    FEAT: Add some bottom space for discharge tab

--- src/pages/hospital/inpatient/[id]/index.js
+++ src/pages/hospital/inpatient/[id]/index.js
@@ -278,7 +278,7 @@ const InpatientDetails = () => {
           loading={patientLoading}
           refetch={refetchPatient}
         />
-        <Card sx={{ mt: 6, p: { xs: 3, md: 6 } }}>
+        <Card sx={{ mt: 6, p: { xs: 3, md: 6 }, mb: selectedTab === 'discharge' ? 4 : 0 }}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
               <IconButton size='large' edge='start' color='inherit' aria-label='menu' onClick={handleMenuOpen}>
