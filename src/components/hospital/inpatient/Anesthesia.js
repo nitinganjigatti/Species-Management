94eb5777bd3ceb45991d079c5a7147d5c0952379
samlifescience <sandeep.mandal@lifesciencetrust.com>
2025-12-04 15:12:15 +0530

    removed refetch api if tab is active o anesthesia details tab in inpatient details page

--- src/components/hospital/inpatient/Anesthesia.js
+++ src/components/hospital/inpatient/Anesthesia.js
@@ -563,14 +563,16 @@ function Anesthesia({
   const handleRecordTabClick = useCallback(
     selectionId => {
       if (selectionId === activeRecordId) {
-        refetchAnesthesiaDetail()
-
+        // refetchAnesthesiaDetail()
         return
       }
 
       setActiveRecordId(selectionId)
     },
-    [activeRecordId, refetchAnesthesiaDetail]
+    [
+      activeRecordId
+      // refetchAnesthesiaDetail
+    ]
   )
 
   const handleDeleteClick = useCallback(() => {
