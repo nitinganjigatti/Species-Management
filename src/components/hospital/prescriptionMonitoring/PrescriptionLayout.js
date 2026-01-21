c06b9739d308e53fcd2cca558daed8a5e9155bce
Naseer <naseer@trellisys.net>
2026-01-21 16:58:30 +0530

    Passing hospital id to get prescription details.

--- src/components/hospital/prescriptionMonitoring/PrescriptionLayout.js
+++ src/components/hospital/prescriptionMonitoring/PrescriptionLayout.js
@@ -252,7 +252,10 @@ function PrescriptionLayout({ drawerType, overviewData, category }) {
         prescription_id: data?.id || medicineDetails?.prescription_id,
         date: data?.customDate || detailSelectedDate || selectedDate,
         group_prescription_id: data?.id || medicineDetails?.prescription_id,
-        administrative_ids: data?.administrative_ids || administrativeIds || ''
+        administrative_ids: data?.administrative_ids || administrativeIds || '',
+
+        // request_from: 'hospital',
+        hospital_id: hospital?.id || ''
 
         // medical_record_id: data?.medical_record_id || medicineDetails?.medical_record_id,
         // medicine_id: data?.medicine_id || medicineDetails?.medicine_id
