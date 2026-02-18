040238c11009727ea7b3de9f43618df3706c35e5
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2026-02-18 17:34:33 +0530

    UPDATE: AID fallback if the local identifier name is not present in AnimalInfoCard

--- src/pages/hospital/inpatient/AddSurgeryRecord.js
+++ src/pages/hospital/inpatient/AddSurgeryRecord.js
@@ -1054,7 +1054,7 @@ const AddSurgeryRecord = () => {
 
   const handleAIDDisplay = () => {
     if (patientData?.animal_detail?.local_identifier_name && patientData?.animal_detail?.local_identifier_value) {
-      return `${patientData?.animal_detail?.local_identifier_name}: ${patientData?.animal_detail?.local_identifier_value}`
+      return patientData?.animal_detail?.local_identifier_value
     } else {
       return patientData?.animal_detail?.animal_id
     }
@@ -1125,7 +1125,13 @@ const AddSurgeryRecord = () => {
           age={`${patientData?.animal_detail?.age}`}
           gender={`${patientData?.animal_detail?.sex}`}
           additionalFields={[
-            { label: 'AID', value: handleAIDDisplay() },
+            {
+              label:
+                patientData?.animal_detail?.local_identifier_name && patientData?.animal_detail?.local_identifier_value
+                  ? patientData?.animal_detail?.local_identifier_name
+                  : 'AID',
+              value: handleAIDDisplay()
+            },
             { label: 'Health Status', value: patientData?.health_status || 'stable', isStatusCard: true },
             // { label: 'Admitted days', value: patientData?.admitted_for_day },
             { label: 'Holding Location', value: `${patientData?.bed_name}, ${patientData?.room_name}` },
