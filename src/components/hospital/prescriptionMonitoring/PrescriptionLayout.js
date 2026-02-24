1d5de51f5dabe907f85f5e525af9e497c9774b9b
BasavarajLifeScience <basavaraj@lifesciencetrust.com>
2026-02-24 15:31:25 +0530

    UPDATE: Sort prescription measurement type and delivery route items

--- src/components/hospital/prescriptionMonitoring/PrescriptionLayout.js
+++ src/components/hospital/prescriptionMonitoring/PrescriptionLayout.js
@@ -743,7 +743,7 @@ function PrescriptionLayout({ drawerType, overviewData, category }) {
               value: item.key,
               unit_name: item.label,
               uom_abbr: item.key
-            })) || [],
+            }))?.sort((a, b) => a.label?.localeCompare(b.label)) || [],
           prescriptionDuration: response?.data?.prescriptionDuration?.map(item => ({ ...item, value: item.key })) || [],
           prescriptionMeasurementType:
             response?.data?.prescriptionMeasurementType?.map(item => ({
@@ -756,7 +756,7 @@ function PrescriptionLayout({ drawerType, overviewData, category }) {
               ...item,
               label: item.delivery,
               value: item.route_abbr
-            })) || []
+            }))?.sort((a, b) => a.label?.localeCompare(b.label)) || []
         })
       } else {
         setMedicalMasterData([])
