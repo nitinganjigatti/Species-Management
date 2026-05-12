306f76fda0921f44ff9e5c7a4907fc491fe5c948
Nidhin Pratap <nidhin@trellisys.net>
2026-05-12 20:19:46 +0530

    Revert "REFACTOR: Use shared AnimalCard in animals listing; drop redundant Location and Born columns"
    This reverts commit 9cca3c1607025d97d0ccc8179c6c305e92cda11f.

--- src/views/pages/compliance/dashboard/AnimalsListingView.js
+++ src/views/pages/compliance/dashboard/AnimalsListingView.js
@@ -14,8 +14,7 @@ const AnimalsListingView = ({
   onClearAll,
   loading,
   onRowClick,
-  onExportClick,
-  rowHeight
+  onExportClick
 }) => (
   <Box>
     <Stack direction='row' justifyContent='space-between' alignItems='flex-start' mb={4} flexWrap='wrap' gap={2}>
@@ -62,7 +61,6 @@ const AnimalsListingView = ({
         handleSearch={handleSearch}
         loading={loading}
         onRowClick={onRowClick}
-        rowHeight={rowHeight}
       />
     </Card>
   </Box>
