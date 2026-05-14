2a6bf31305fdbba7e3d729ad28683393e6332cce
saket-lset <saket@lifesciencetrust.com>
2026-05-14 14:48:14 +0530

    Converted Analytics Page from page routing to app routing. Also javascript to typescript

---
+++ src/app/(module)/analytics/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Box from '@mui/material/Box'
+import AnalyticsPage from 'src/components/analytics/AnalyticsPage'
+
+const Analytics = () => (
+  <Box sx={{ minHeight: '100vh' }}>
+    <AnalyticsPage />
+  </Box>
+)
+
+export default Analytics
