b5ebb066f05dee0e27800f1d7568aab4f65ae70a
saket-lset <saket@lifesciencetrust.com>
2026-05-15 15:58:04 +0530

    User media converted to typescript and app routing

--- src/lib/api/utility/index.d.ts
+++ src/lib/api/utility/index.d.ts
@@ -4,5 +4,11 @@ export declare const axiosPost: (options: { url: string; body?: any; pharmacy?: 
 export declare const axiosFormPost: (options: { url: string; body?: any; pharmacy?: any }) => Promise<any>
 export declare const axiosDelete: (options: { url: string; params?: any; pharmacy?: any }) => Promise<any>
 export declare const axiosGetExternal: (options: { url: string; params?: any; pharmacy?: any }) => Promise<any>
-export declare const axiosAuthFormPost: (options: { url: string; body?: any; pharmacy?: any; authToken?: any }) => Promise<any>
+export declare const axiosAuthFormPost: (options: {
+  url: string
+  body?: any
+  pharmacy?: any
+  authToken?: any
+}) => Promise<any>
 export declare const axiosMLPost: (options: { url: string; data?: any }) => Promise<any>
+export declare const fetchFormPostMedia: (options: { url: string; body?: any; pharmacy?: any }) => Promise<any>
---
+++ src/types/media/index.ts
@@ -0,0 +1,5 @@
+// ==================== Models ====================
+export type { MediaItem, MediaGroup } from './models'
+
+// ==================== API ====================
+export type { GetMediaParams, UploadMediaPayload } from './api'
